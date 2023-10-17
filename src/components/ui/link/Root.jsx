0846d82ed2c15fbbf8acb9e114f95afcb302712e
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-17 19:17:19 -0300

    Refactoring link design and code

--- src/components/ui/link/Root.jsx
+++ src/components/ui/link/Root.jsx
@@ -10,18 +10,17 @@ const Link = ({ href, className, ...props }, ref) => {
 
   const externalLinkProps = { rel: 'noreferrer', target: '_blank' };
 
-  props = {
-    className: cn(
-      'cursor-pointer font-bold inline-flex items-center justify-center',
-      className,
-    ),
-    href,
-    ref,
-    ...(isExternal ? externalLinkProps : {}),
-    ...props,
-  };
-
-  return <NextLink {...props} />;
+  return (
+    <NextLink
+      className={cn(
+        'inline-flex cursor-pointer items-center justify-center gap-[.25em] font-semibold',
+        className,
+      )}
+      href={href}
+      ref={ref}
+      {...(isExternal && externalLinkProps)}
+      {...props}
+    />
+  );
 };
-
 export default forwardRef(Link);
