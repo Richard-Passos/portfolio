70b27c5e71ba74b1fd6b9983af2bb7035f753503
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-20 19:45:29 -0300

    Removing not working components and errors

--- src/components/ui/link/Root.jsx
+++ src/components/ui/link/Root.jsx
@@ -3,22 +3,17 @@
 import NextLink from 'next/link';
 import { forwardRef } from 'react';
 
-import { cn, isExternalUrl } from '@/utils';
+import { cn } from '@/utils';
 
 const Link = ({ href, className, ...props }, ref) => {
-  const isExternal = isExternalUrl(href);
-
-  const externalLinkProps = { rel: 'noreferrer', target: '_blank' };
-
   return (
     <NextLink
       className={cn(
-        'inline-flex cursor-pointer items-center justify-center gap-[.25em] font-semibold',
+        'inline-flex cursor-pointer items-center justify-center gap-x-[--font-blank-space] font-semibold',
         className,
       )}
       href={href}
       ref={ref}
-      {...(isExternal && externalLinkProps)}
       {...props}
     />
   );
