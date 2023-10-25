26dd0a0c07030ecbb9d845c010c25bda487cb158
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-25 14:42:25 -0300

    Refactorign button design

--- src/components/ui/button/Root.jsx
+++ src/components/ui/button/Root.jsx
@@ -1,7 +1,4 @@
-'use client';
-
 import { Slot } from '@radix-ui/react-slot';
-import { motion } from 'framer-motion';
 import { forwardRef } from 'react';
 
 import { cn, cnv } from '@/utils';
@@ -9,20 +6,19 @@ import { cn, cnv } from '@/utils';
 import variantsColor from '../variantsColor';
 
 const Button = ({ asChild, variants, className, ...props }, ref) => {
-  const Tag = asChild ? MotionChild : motion.button;
+  const Tag = asChild ? Slot : 'button';
 
   return (
     <Tag
       className={cn(buttonVariants(variants), className)}
       ref={ref}
-      whileTap={{ '--tw-scale-x': 0.9, '--tw-scale-y': 0.9 }}
       {...props}
     />
   );
 };
 
 const buttonVariants = cnv({
-  base: 'transition-color transition-bg inline-flex items-center justify-center gap-x-[--font-blank-space] rounded-lg border border-transparent font-medium disabled:pointer-events-none disabled:opacity-50',
+  base: 'inline-flex items-center justify-center gap-x-font-blank-space rounded-full border border-transparent font-medium transition-colors disabled:pointer-events-none disabled:opacity-50',
   variants: {
     color: variantsColor,
     variant: {
@@ -43,8 +39,6 @@ const buttonVariants = cnv({
     size: 'md',
   },
 });
-
-const MotionChild = motion(Slot);
 
 export default forwardRef(Button);
 export { buttonVariants };
