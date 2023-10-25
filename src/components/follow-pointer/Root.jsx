c5e1298115d2c0b8e643276b83c019acfcdabe3d
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-25 20:30:32 -0300

    summing scollY to y axis

--- src/components/follow-pointer/Root.jsx
+++ src/components/follow-pointer/Root.jsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { Slot } from '@radix-ui/react-slot';
-import { motion, useTransform } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { useScrollerMotion } from 'scroller-motion';
 
 import { useFollowPointer, useSmooth } from '@/hooks';
 import { cn, transformTemplate } from '@/utils';
@@ -12,6 +13,8 @@ const FollowPointer = ({ id, smoothConfig, className, style, ...props }) => {
   const ref = useRef(null),
     pointerPos = useFollowPointer(ref),
     { showList } = useSelector((data) => data['follow-pointer']);
+
+  const { scrollY } = useScrollerMotion();
 
   const isShowing = showList.includes(id);
 
@@ -28,13 +31,15 @@ const FollowPointer = ({ id, smoothConfig, className, style, ...props }) => {
       smoothConfig,
     ),
     y = useSmooth(
-      useTransform(() => style?.y.get() ?? pointerPos.y.get()),
+      useTransform(
+        () => (style?.y.get() ?? pointerPos.y.get()) + scrollY.get(),
+      ),
       smoothConfig,
     );
 
   return (
     <MotionChild
-      className={cn('pointer-events-none fixed z-[99]', className)}
+      className={cn('pointer-events-none absolute z-[99]', className)}
       ref={ref}
       style={{
         ...style,
