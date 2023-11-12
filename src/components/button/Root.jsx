6cacdab362d0dcec845c5e70a47fea7cbf1dbd6b
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-12 18:19:28 -0300

    Removing sticky animation from project and refactoring button

---
+++ src/components/button/Root.jsx
@@ -0,0 +1,31 @@
+import { forwardRef } from 'react';
+
+import Magnetic from '../magnetic';
+import { Button as ButtonUi, Link } from '../ui';
+
+const Button = ({ limit = 0.5, children, isLink, variants, ...props }, ref) => {
+  const content = (
+    <Magnetic limit={limit - 0.1}>
+      <span className='flex h-full w-full items-center justify-center rounded-inherit'>
+        {children}
+      </span>
+    </Magnetic>
+  );
+
+  return (
+    <Magnetic
+      limit={limit}
+      ref={ref}
+      {...props}
+    >
+      <ButtonUi
+        asChild={isLink}
+        variants={variants}
+      >
+        {isLink ? <Link>{content}</Link> : content}
+      </ButtonUi>
+    </Magnetic>
+  );
+};
+
+export default forwardRef(Button);
---
+++ src/components/button/index.js
@@ -0,0 +1,3 @@
+import Button from './Root';
+
+export default Button;
