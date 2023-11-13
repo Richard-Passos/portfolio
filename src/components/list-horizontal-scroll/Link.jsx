5b89f5da36358c51c0bd76608b37d1ac658f9226
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-13 20:19:35 -0300

    Adding the correct transition to list-horizontal-scroll-Link

--- src/components/list-horizontal-scroll/Link.jsx
+++ src/components/list-horizontal-scroll/Link.jsx
@@ -6,7 +6,7 @@ const ListHorizontalScrollLink = ({ className, variants, ...props }) => {
   return (
     <Button
       className={cn(
-        'tracking-widest shadow-[0_0_50px_-15px_var(--tw-shadow-color)] shadow-[hsl(0_0%_80%/.25)] transition-[background-color,color,border,box-shadow] dark:shadow-[hsl(0_0%_5%/.25)]',
+        'tracking-widest shadow-[0_0_50px_-15px_var(--tw-shadow-color)] shadow-[hsl(0_0%_80%/.25)] transition-[color,background-color,border-color,text-decoration-color,box-shadow,text-underline-offset] dark:shadow-[hsl(0_0%_5%/.25)]',
         className,
       )}
       isLink
