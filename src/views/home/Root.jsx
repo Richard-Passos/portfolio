21fdc9df9b15f37caf4a63d1812ac7ffe56578ce
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-13 15:54:11 -0300

    Adding text and theme management on home root.jsx

--- src/views/home/Root.jsx
+++ src/views/home/Root.jsx
@@ -1,13 +1,59 @@
+import { Section } from '@/components';
+import { Text } from '@/components/ui/text';
+
 import Sections from './sections';
 
 const HomeView = () => {
   return (
     <main className='relative mx-auto max-w-bounds'>
-      <Sections.Hero />
+      <Sections.Hero theme='light' />
 
-      <Sections.Work />
+      <Section
+        asChild
+        className='flex items-center justify-center'
+        id='scroll-down-to'
+        theme='light'
+      >
+        <div>
+          <Text className='w-[86%] text-center text-5xl'>
+            When it comes to frontend, attention to detail is the name of the
+            game. I got that eagle eye that can spot even the smallest
+            imperfections and make sure your user experience is smooth like
+            butter.
+          </Text>
+        </div>
+      </Section>
 
-      <Sections.About />
+      <Sections.Work theme='dark' />
+
+      <Section
+        asChild
+        className='flex items-center justify-center'
+        theme='light'
+      >
+        <div>
+          <Text className='flex w-[86%] items-center justify-center text-center text-8xl'>
+            <span className='outline-text absolute text-[16rem] uppercase'>
+              but
+            </span>{' '}
+            it&apos;s not just about tech.
+          </Text>
+        </div>
+      </Section>
+
+      <Sections.About theme='dark' />
+
+      <Section
+        asChild
+        className='flex items-center justify-center'
+      >
+        <div>
+          <Text className='w-[86%] text-center text-5xl'>
+            If you&apos;re looking for a developer who&apos;s got the skills,
+            the creativity, and the style. Then you need to work with me.
+          </Text>
+        </div>
+      </Section>
     </main>
   );
 };
