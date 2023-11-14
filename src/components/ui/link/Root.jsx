d4dcc7214236fa07a1afee790ea274660408fd47
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-14 15:02:25 -0300

    Adding asChild prop to link ui

--- src/components/ui/link/Root.jsx
+++ src/components/ui/link/Root.jsx
@@ -1,18 +1,20 @@
 'use client';
 
+import { Slot } from '@radix-ui/react-slot';
 import NextLink from 'next/link';
 import { forwardRef } from 'react';
 
 import { useIsExternalUrl } from '@/hooks';
 import { cn } from '@/utils';
 
-const Link = ({ href, className, ...props }, ref) => {
+const Link = ({ href, asChild, className, ...props }, ref) => {
   const isExternal = useIsExternalUrl(href);
 
-  const externalLinkProps = { rel: 'noreferrer', target: '_blank' };
+  const Tag = asChild ? Slot : NextLink,
+    externalLinkProps = { rel: 'noreferrer', target: '_blank' };
 
   return (
-    <NextLink
+    <Tag
       className={cn(
         'inline-flex cursor-pointer items-center justify-center gap-font-blank-space font-semibold underline decoration-transparent underline-offset-0 transition-[color,background-color,border-color,text-decoration-color,text-underline-offset] hover:decoration-current hover:underline-offset-4',
         className,
