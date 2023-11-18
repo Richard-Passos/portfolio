af1ba98b5590bd94ff7b08bb5cce32b7af856b43
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-18 11:01:22 -0300

    Changing animation from scroll to in-view

--- src/components/text-scroll-animation/Root.jsx
+++ src/components/text-scroll-animation/Root.jsx
@@ -1,65 +1,17 @@
-import ScrollAnimation from '../scroll-animation';
-import { scrollSmoothConfig } from '../smooth-scroll';
+import Char from './Char';
 
-const TextScrollAnimation = ({ text, className, ...props }) => {
-  const words = text.split(' ');
+const TextScrollAnimation = ({ text, ...props }) => {
+  const chars = text.split('');
 
-  return (
-    <>
-      {words.map((word, i) => (
-        <>
-          <Word
-            key={i}
-            {...props}
-          >
-            {word}
-          </Word>{' '}
-        </>
-      ))}
-    </>
-  );
-};
-
-const Word = ({ children, index, ...props }) => {
-  const defaultProps = {
-    smoothConfig: {
-      scroll: scrollSmoothConfig,
-    },
-  };
-
-  const animationConfig = {
-    opacity: {
-      useScrollConfig: {
-        offset: ['2 1', '3 1'],
-      },
-      useScrollRes: 'scrollYProgress',
-      prop: '--opacity',
-      scrollPoints: [0, 1],
-      propPoints: [0, 1],
-    },
-    y: {
-      useScrollConfig: {
-        offset: ['1 1', '4 1'],
-      },
-      propPoints: ['100%', '0%'],
-    },
-  };
-
-  return (
-    <ScrollAnimation
-      config={animationConfig.opacity}
-      {...defaultProps}
+  return chars.map((char, i) => (
+    <Char
+      index={i}
+      key={i}
       {...props}
     >
-      <ScrollAnimation.Transform
-        className='opacity-[--opacity]'
-        config={animationConfig.y}
-        {...defaultProps}
-      >
-        <span>{children}</span>
-      </ScrollAnimation.Transform>
-    </ScrollAnimation>
-  );
+      {char}
+    </Char>
+  ));
 };
 
 export default TextScrollAnimation;
