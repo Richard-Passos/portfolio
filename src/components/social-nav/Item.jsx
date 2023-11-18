0a6c6c42220946cc470794c3d1eb7f93ac236e32
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-18 20:53:41 -0300

    Creating new animation to social-nav

--- src/components/social-nav/Item.jsx
+++ src/components/social-nav/Item.jsx
@@ -1,15 +1,32 @@
 import { capitalize, cn } from '@/utils';
 
+import { AnimationInView } from '../animation';
 import { MagneticLink } from '../link';
 
-const SocialNavItem = ({ name, className, ...props }) => {
+const SocialNavItem = ({ name, className, href, ...props }) => {
+  const animation = {
+    variants: {
+      hidden: { opacity: 0, y: '100%' },
+      visible: {
+        opacity: 1,
+        y: '0%',
+      },
+    },
+  };
+
   return (
-    <MagneticLink
-      className={cn('rounded-sm px-3 py-1.5 text-sm', className)}
+    <AnimationInView
+      animation={animation}
+      className={cn(
+        'text-sm focus-within:!translate-y-0 focus-within:!opacity-100',
+        className,
+      )}
       {...props}
     >
-      {capitalize(name)}
-    </MagneticLink>
+      <div>
+        <MagneticLink className='aspect-square h-10 rounded-sm' href={href}>{capitalize(name)}</MagneticLink>
+      </div>
+    </AnimationInView>
   );
 };
 
--- src/components/social-nav/Root.jsx
+++ src/components/social-nav/Root.jsx
@@ -1,11 +1,38 @@
-import { cn } from '@/utils';
+'use client';
 
-const SocialNav = ({ className, ...props }) => {
+import { Share1Icon } from '@radix-ui/react-icons';
+
+import { cn, transformTemplate } from '@/utils';
+
+import { Animation } from '../animation';
+import { Text } from '../ui/text';
+
+const SocialNav = ({ className, children, ...props }) => {
+  const variants = [
+    { y: '4.5rem' },
+    { y: '1rem', transition: { staggerChildren: 0.1 } },
+  ];
+
   return (
-    <nav
-      className={cn('flex gap-3', className)}
+    <Animation
+      className={cn(
+        'group translate-x-4 space-y-4 p-4 focus-within:!translate-y-4',
+        className,
+      )}
       {...props}
-    />
+      transformTemplate={transformTemplate}
+      type='whileHover'
+      variants={variants}
+    >
+      <div>
+        <Text className='ml-auto flex items-center gap-4 text-xs font-bold uppercase text-content transition-transform group-hover:translate-x-[1.875rem]'>
+          Follow me{' '}
+          <Share1Icon className='h-3.5 w-3.5 transition-opacity group-hover:opacity-0' />
+        </Text>
+
+        <nav className='flex w-max gap-3'>{children}</nav>
+      </div>
+    </Animation>
   );
 };
 export default SocialNav;
