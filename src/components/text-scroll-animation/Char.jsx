f29a8aa90b19d227a3f513d75cf70ecb9441954b
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-18 11:02:06 -0300

    Moving char to a separeted file

---
+++ src/components/text-scroll-animation/Char.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { cn } from '@/utils';
+
+import Animation from '../animation';
+
+const TextScrollAnimationChar = ({ className, index, children, ...props }) => {
+  const variants = [
+    {
+      opacity: 0,
+      x: 60,
+      scaleY: 0,
+    },
+    (i) => ({
+      opacity: 1,
+      x: 0,
+      scaleY: 1,
+      transition: {
+        delay: 0.01 * i,
+      },
+    }),
+  ];
+
+  return (
+    <Animation.InView
+      className={cn('min-w-[--font-blank-space]', className)}
+      custom={index}
+      variants={variants}
+      {...props}
+    >
+      <span>{children}</span>
+    </Animation.InView>
+  );
+};
+
+export default TextScrollAnimationChar;
