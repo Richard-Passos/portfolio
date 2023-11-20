f610d1011e8ffc3f09b931e2c3ffeed3dfe87858
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-20 17:52:43 -0300

    Exporting new hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,4 +2,6 @@ export { default as useChangeKeyWhenFalsy } from './useChangeKeyWhenFalsy';
 export { default as useFollowPointer } from './useFollowPointer';
 export { default as useFormField } from './useFormField';
 export { default as useIsExternalUrl } from './useIsExternalUrl';
+export { default as useLocalTime } from './useLocalTime';
 export { default as useSmooth } from './useSmooth';
+export { default as useToast } from './useToast';
