6bfe098fef3fe8398e9d156c01f1aad801935d69
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-20 17:52:31 -0300

    Changing socail-medias text

--- src/constants/socialMedias.js
+++ src/constants/socialMedias.js
@@ -1,15 +1,15 @@
 const socialMedias = [
   {
     href: 'https://github.com/Richard-Passos',
-    name: 'GH',
+    name: 'Gh',
   },
   {
     href: 'https://www.instagram.com/richardp.dev',
-    name: 'IN',
+    name: 'Ig',
   },
   {
     href: 'https://www.linkedin.com/in/richardp-dev',
-    name: 'LN',
+    name: 'In',
   },
 ];
 
