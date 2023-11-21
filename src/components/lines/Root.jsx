1463db0e5a09acee338ff5ed77ed64a9faf1c259
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-21 16:25:59 -0300

    Adding an animation for changing themes on lines

--- src/components/lines/Root.jsx
+++ src/components/lines/Root.jsx
@@ -6,16 +6,27 @@ const Lines = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-around opacity-40 transition-opacity dark:opacity-20',
+        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-around',
         className,
       )}
       {...props}
     >
       {[...Array(LINES_AMOUNT)].map((_, i) => (
-        <span
-          className='h-full w-px bg-content opacity-10 transition-opacity dark:opacity-5'
+        <div
+          className='relative h-full w-full overflow-hidden'
           key={i}
-        />
+        >
+          <div
+            className={cn(
+              '[.dark_&]:animate-slide-dark [.light_&]:animate-slide-light absolute right-0 flex h-full w-[200%] gap-px',
+            )}
+          >
+            <span className='[.dark_&]:light dark h-full w-1/2 bg-main delay-150' />
+            <span className='light [.dark_&]:dark h-full w-1/2 bg-main delay-150' />
+          </div>
+
+          <span className='absolute inset-0 w-auto border-l opacity-20 transition-opacity dark:opacity-10' />
+        </div>
       ))}
     </div>
   );
