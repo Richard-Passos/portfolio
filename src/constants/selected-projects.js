9b960f8073c7dd695f3c9f993454f14f6d33f473
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-22 21:28:32 -0300

    Creating selected-projects structure

---
+++ src/constants/selected-projects.js
@@ -0,0 +1,54 @@
+const selectedProjects = [
+  {
+    href: '#',
+    title: 'PROJECT-01',
+    role: 'Design & Development',
+    img: {
+      src: 'https://picsum.photos/500/300?random=1',
+      alt: 'Project 01 image',
+      fill: true,
+    },
+  },
+  {
+    href: '#',
+    title: 'PROJECT-02',
+    role: 'Design & Development',
+    img: {
+      src: 'https://picsum.photos/500/300?random=2',
+      alt: 'Project 02 image',
+      fill: true,
+    },
+  },
+  {
+    href: '#',
+    title: 'PROJECT-03',
+    role: 'Design & Development',
+    img: {
+      src: 'https://picsum.photos/500/300?random=3',
+      alt: 'Project 03 image',
+      fill: true,
+    },
+  },
+  {
+    href: '#',
+    title: 'PROJECT-04',
+    role: 'Design & Development',
+    img: {
+      src: 'https://picsum.photos/500/300?random=4',
+      alt: 'Project 04 image',
+      fill: true,
+    },
+  },
+  {
+    href: '#',
+    title: 'PROJECT-05',
+    role: 'Design & Development',
+    img: {
+      src: 'https://picsum.photos/500/300?random=5',
+      alt: 'Project 05 image',
+      fill: true,
+    },
+  },
+];
+
+export default selectedProjects;
