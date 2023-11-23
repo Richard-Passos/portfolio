f022fe5f8adfdb2196e543cecb386b7eb9faa542
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-22 21:32:31 -0300

    Applying new desing on work and contact sections with new components and bento-grids

--- src/views/home/sections/About.jsx
+++ src/views/home/sections/About.jsx
@@ -1,37 +1,138 @@
-import { ListHorizontalScroll, Section } from '@/components';
-import { Text } from '@/components/ui/text';
+import { ClockIcon, GlobeIcon } from '@radix-ui/react-icons';
+
+import {
+  ChangeTheme,
+  ListHorizontalScroll,
+  LocalTime,
+  ScrollTitle,
+  Section,
+} from '@/components';
+import { Badge } from '@/components/ui';
+import { Text, TextTitle } from '@/components/ui/text';
 import { cn } from '@/utils';
 
-const listContent = ['Personality', 'Hobbys', 'Background'];
-
-const HomeAboutSection = ({ className, ...props }) => {
+const HomeAboutSection = ({ className, theme, ...props }) => {
   return (
     <Section
-      className={cn('relative flex items-center justify-center', className)}
+      className={cn(
+        'relative flex flex-col items-center gap-24 2xl:h-auto 2xl:max-h-none',
+        className,
+      )}
+      {...props}
+    >
+      <ScrollTitle title='ABOUT' />
+
+      <BentoGrids />
+
+      <ListContactPage />
+
+      <ChangeTheme
+        className='pointer-events-none absolute left-0 top-0 h-full max-h-screen w-full'
+        theme={theme}
+      >
+        <span />
+      </ChangeTheme>
+    </Section>
+  );
+};
+
+const BentoGrids = ({ className, ...props }) => {
+  return (
+    <ul
+      className={cn(
+        'grid w-full max-w-screen-lg grid-cols-3 gap-4 [grid-template-areas:"._item-1_item-1""item-2_item-3_item-4""item-5_item-5_item-4"]',
+        className,
+      )}
+      {...props}
+    >
+      <BentoGridsItem className='flex-col justify-between [grid-area:item-1]'>
+        <Badge
+          className='w-fit'
+          variants={{ color: 'inverted', style: 'outline' }}
+        >
+          #about
+        </Badge>
+
+        <Text>
+          👋 Hi — I&apos;m Richard Passos, a full stack developer based in
+          Brazil. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum,
+          consequatur? Distinctio deserunt iure repudiandae ducimus!.
+        </Text>
+      </BentoGridsItem>
+
+      <BentoGridsItem className='flex-col justify-between [grid-area:item-2]'>
+        <GlobeIcon className='h-8 w-8' />
+
+        <Text className='text-4xl'>Based in Brazil, GMT-3</Text>
+      </BentoGridsItem>
+
+      <BentoGridsItem className='[grid-area:item-3]' />
+
+      <BentoGridsItem className='h-full [grid-area:item-4]' />
+
+      <BentoGridsItem className='items-center justify-center gap-24 [grid-area:item-5]'>
+        <ClockIcon className='h-40 w-40 [stroke:theme(spacing.px)]' />
+
+        <section className='flex flex-col items-center gap-2 text-center'>
+          <Text>my local time</Text>
+
+          <TextTitle className='text-4xl font-bold'>
+            <LocalTime />
+          </TextTitle>
+
+          <Text>22/11/2023</Text>
+        </section>
+      </BentoGridsItem>
+    </ul>
+  );
+};
+
+const BentoGridsItem = ({ className, ...props }) => {
+  return (
+    <li
+      className={cn(
+        'flex h-60 rounded-md border bg-main p-6 transition-bg',
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+const ListContactPage = ({ className, ...props }) => {
+  const content = ['Personality', 'Hobbys', 'Background'];
+
+  return (
+    <div
+      className={cn(
+        'relative flex w-full items-center justify-center',
+        className,
+      )}
       {...props}
     >
       <ListHorizontalScroll>
-        {listContent.map((content, i) => (
+        {content.map((content, i) => (
           <ListHorizontalScroll.Item
-            baseVelocity={i % 2 === 0 ? -100 : 100}
+            baseVelocity={i % 2 === 1 ? 5 : -5}
+            className='odd:rotate-[.5deg] even:-rotate-[.5deg] [&>div>div]:gap-8'
             key={content}
           >
-            <div className='mx-4 flex items-center gap-8 opacity-75'>
-              <Text>{content}</Text>
+            <Text className='outline-text mr-8 [-webkit-text-stroke-width:theme(spacing.px)]'>
+              {content}
+            </Text>
 
-              <span className='aspect-square w-[.2em] rounded-full bg-current' />
-            </div>
+            <span className='aspect-square w-[.2em] rounded-full border border-current' />
           </ListHorizontalScroll.Item>
         ))}
       </ListHorizontalScroll>
 
       <ListHorizontalScroll.Link
         className='absolute'
-        href='/about'
+        href='/contact'
       >
         Explore
       </ListHorizontalScroll.Link>
-    </Section>
+    </div>
   );
 };
 
--- src/views/home/sections/Work.jsx
+++ src/views/home/sections/Work.jsx
@@ -1,26 +1,93 @@
-import { ListHorizontalScroll, Section } from '@/components';
+import { PlusIcon } from '@radix-ui/react-icons';
+
+import {
+  Button,
+  ChangeTheme,
+  ListHorizontalScroll,
+  ListProjects,
+  ScrollTitle,
+  Section,
+} from '@/components';
 import { Text } from '@/components/ui/text';
+import { selectedProjects } from '@/constants';
 import { cn } from '@/utils';
 
-const listContent = ['Work method', 'Soft & hard skills', 'Projects'];
-
-const HomeWorkSection = ({ className, ...props }) => {
+const HomeWorkSection = ({ className, theme, ...props }) => {
   return (
     <Section
-      className={cn('relative flex items-center justify-center', className)}
+      className={cn(
+        'relative flex flex-col items-center gap-24 2xl:h-auto 2xl:max-h-none',
+        className,
+      )}
+      {...props}
+    >
+      <ScrollTitle title='WORK' />
+
+      <ListProjects>
+        {selectedProjects.map((project, i) => (
+          <ListProjects.Item
+            index={i}
+            key={project.href}
+            {...project}
+          />
+        ))}
+      </ListProjects>
+
+      <Button
+        className='relative -mt-12'
+        href='/projects'
+        isLink
+      >
+        <PlusIcon
+          aria-hidden
+          className='h-10 w-10'
+        />
+
+        <span className='sr-only'>More projects</span>
+      </Button>
+
+      <ListWorkPage />
+
+      <ChangeTheme
+        className='pointer-events-none absolute left-0 top-0 h-1/2 max-h-screen w-full'
+        theme={theme}
+      >
+        <span />
+      </ChangeTheme>
+
+      <ChangeTheme
+        className='pointer-events-none absolute bottom-0 left-0 h-1/2 max-h-screen w-full'
+        theme={theme}
+      >
+        <span />
+      </ChangeTheme>
+    </Section>
+  );
+};
+
+const ListWorkPage = ({ className, ...props }) => {
+  const content = ['Work method', 'Soft & hard skills', 'Projects'];
+
+  return (
+    <div
+      className={cn(
+        'relative flex w-full items-center justify-center',
+        className,
+      )}
       {...props}
     >
       <ListHorizontalScroll>
-        {listContent.map((content, i) => (
+        {content.map((content, i) => (
           <ListHorizontalScroll.Item
-            baseVelocity={i % 2 === 0 ? 100 : -100}
+            baseVelocity={i % 2 === 0 ? 5 : -5}
+            className='[&>div>div]:gap-8'
             key={content}
           >
-            <div className='mx-4 flex items-center gap-8 opacity-75'>
-              <Text>{content}</Text>
+            <Text className='outline-text mr-8 [-webkit-text-stroke-width:theme(spacing.px)]'>
+              {content}
+            </Text>
 
-              <span className='aspect-square w-[.2em] rounded-full bg-current' />
-            </div>
+            <span className='aspect-square w-[.2em] rounded-full border border-current' />
           </ListHorizontalScroll.Item>
         ))}
       </ListHorizontalScroll>
@@ -31,7 +98,7 @@ const HomeWorkSection = ({ className, ...props }) => {
       >
         Explore
       </ListHorizontalScroll.Link>
-    </Section>
+    </div>
   );
 };
 
