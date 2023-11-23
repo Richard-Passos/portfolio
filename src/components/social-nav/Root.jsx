3ed5e180b111b8b5af23d9639814aedb0bf99c96
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-22 21:42:49 -0300

    Setting duration to 300ms and changing icon animation to scale instead of scale-x

--- src/components/social-nav/Root.jsx
+++ src/components/social-nav/Root.jsx
@@ -10,16 +10,16 @@ const SocialNav = ({ className, ...props }) => {
   return (
     <section
       className={cn(
-        'group translate-x-4 translate-y-[4.5rem] space-y-4 p-4 transition-transform [--icon-scale-x:1] [--item-opacity:0] [--item-y:100%] [--title-x:0%] focus-within:translate-y-4 focus-within:[--icon-scale-x:0] focus-within:[--item-opacity:1] focus-within:[--item-y:0%] focus-within:[--title-x:2.375rem] hover:translate-y-4',
+        'group translate-x-4 translate-y-[4.5rem] space-y-4 p-4 transition-transform duration-300 [--icon-scale:1] [--item-opacity:0] [--item-y:100%] [--title-x:0%] focus-within:translate-y-4 focus-within:[--icon-scale:0] focus-within:[--item-opacity:1] focus-within:[--item-y:0%] focus-within:[--title-x:2.375rem] hover:translate-y-4',
         className,
       )}
       {...props}
     >
       <Text className='ml-auto flex items-center gap-6 text-xs font-bold uppercase'>
-        <span className='translate-x-[--title-x] transition-transform group-hover:translate-x-[2.375rem]'>
+        <span className='translate-x-[--title-x] transition-transform duration-300 group-hover:translate-x-[2.375rem]'>
           Get in touch
         </span>{' '}
-        <span className='origin-right scale-x-[--icon-scale-x] rounded-full bg-content p-1.5 text-main transition-transform group-hover:scale-x-0'>
+        <span className='scale-[--icon-scale] rounded-full bg-content p-1.5 text-main transition-transform duration-300 group-hover:scale-0'>
           <Link2Icon className='h-3.5 w-3.5' />
         </span>{' '}
       </Text>
