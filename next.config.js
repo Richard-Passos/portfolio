db72f63c91aa3c6df1ff055d01517adb68668acc
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-24 18:57:30 -0300

    Removing images placholders config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
