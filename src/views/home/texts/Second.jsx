d93d205979d44ab5488135623e7e41f58ebf3d90
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-23 23:32:07 -0300

    Changing animation and making it more responsiveble

--- src/views/home/texts/Second.jsx
+++ src/views/home/texts/Second.jsx
@@ -5,20 +5,20 @@ import { cn } from '@/utils';
 
 const HomeSecondText = ({ className, theme, ...props }) => {
   const animationConfig = {
-      opacity: {
-        useScrollConfig: {
-          offset: ['1 1', '1.5 1'],
-        },
-        useScrollRes: 'scrollYProgress',
-        prop: 'opacity',
-        scrollPoints: [0, 1],
-        propPoints: [1, 0.05],
-      },
       y: {
         useScrollConfig: {
           offset: ['1 1', '1.5 1'],
         },
         propPoints: ['0%', '50%'],
+      },
+      clipPath: {
+        useScrollConfig: {
+          offset: ['1 1', '2 1'],
+        },
+        useScrollRes: 'scrollYProgress',
+        prop: '--clip-path',
+        scrollPoints: [0, 1],
+        propPoints: ['inset(0% 0 0 0)', 'inset(100% 0 0 0)'],
       },
       x: {
         useScrollConfig: {
@@ -33,35 +33,40 @@ const HomeSecondText = ({ className, theme, ...props }) => {
   return (
     <div
       className={cn(
-        'relative my-[25vh] flex h-[150vh] flex-col items-center justify-end',
+        'relative my-[25vh] flex h-[150vh] flex-col items-center justify-end 2xl:my-[calc(var(--max-h)*.25)] 2xl:max-h-[calc(var(--max-h)*1.5)]',
         className,
       )}
       {...props}
     >
-      <ScrollAnimation
+      <ScrollAnimation.Transform
         className='absolute top-0 flex items-center overflow-hidden'
-        config={animationConfig.opacity}
+        config={animationConfig.y}
         smoothConfig={animationSmoothConfig}
       >
-        <ScrollAnimation.Transform
-          config={animationConfig.y}
-          smoothConfig={animationSmoothConfig}
+        <Section
+          asChild
+          theme={theme}
         >
-          <Section
-            asChild
-            theme={theme}
-          >
-            <div>
+          <div>
+            <ScrollAnimation
+              config={animationConfig.clipPath}
+              smoothConfig={animationSmoothConfig}
+            >
               <ScrollAnimation.Transform
                 config={animationConfig.x}
                 smoothConfig={animationSmoothConfig}
               >
-                <Text className='text-[24rem] font-bold uppercase'>but</Text>
+                <Text
+                  className='outline-text relative text-[24rem] font-bold before:pointer-events-none before:absolute before:origin-bottom before:overflow-hidden before:content-[attr(data-text)] before:[-webkit-text-fill-color:currentColor] before:[clip-path:--clip-path]'
+                  data-text='BUT'
+                >
+                  BUT
+                </Text>
               </ScrollAnimation.Transform>
-            </div>
-          </Section>
-        </ScrollAnimation.Transform>
-      </ScrollAnimation>
+            </ScrollAnimation>
+          </div>
+        </Section>
+      </ScrollAnimation.Transform>
 
       <Section
         asChild
@@ -69,7 +74,7 @@ const HomeSecondText = ({ className, theme, ...props }) => {
         theme={theme}
       >
         <div>
-          <Text className='text-center text-[clamp(2rem,6.6vw,6rem)] leading-tight'>
+          <Text className='text-center text-[clamp(2rem,6.6vw,6rem)] font-bold leading-tight'>
             <TextScrollAnimation lines={["it's not just about the tech."]} />
           </Text>
         </div>
