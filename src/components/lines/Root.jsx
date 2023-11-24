10ed6299ba3c14c72114c5792f1a18ac408e8703
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-23 23:27:04 -0300

    Refactoring code and changing opacity

--- src/components/lines/Root.jsx
+++ src/components/lines/Root.jsx
@@ -1,19 +1,19 @@
 import { cn } from '@/utils';
 
-const LINES_AMOUNT = 6;
+const LINES_AMOUNT = 7;
 
 const Lines = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-around',
+        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-around opacity-40 dark:opacity-10',
         className,
       )}
       {...props}
     >
       {[...Array(LINES_AMOUNT)].map((_, i) => (
         <span
-          className='h-full w-full border-l opacity-40 last:border-r dark:opacity-20'
+          className='h-full w-px bg-border'
           key={i}
         />
       ))}
