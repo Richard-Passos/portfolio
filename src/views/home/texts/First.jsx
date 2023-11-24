39b51a1c3a0d177da39027c8e87e065fb57ec887
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-24 20:12:27 -0300

    Adding name

--- src/views/home/texts/First.jsx
+++ src/views/home/texts/First.jsx
@@ -1,5 +1,7 @@
 import { Section, TextScrollAnimation } from '@/components';
+import { Link } from '@/components/ui';
 import { Text } from '@/components/ui/text';
+import { availabilityMessage } from '@/constants';
 import { cn } from '@/utils';
 
 const HomeFirstText = ({ className, ...props }) => {
@@ -10,7 +12,11 @@ const HomeFirstText = ({ className, ...props }) => {
       {...props}
     >
       <div>
-        <div className='flex w-[90%] max-w-screen-xl justify-end'>
+        <div className='flex w-[90%] max-w-screen-xl justify-between'>
+          <Text className='mb-auto flex -translate-y-12 items-center font-semibold'>
+            Richard Passos — developer
+          </Text>
+
           <Text className='text-[clamp(1rem,2.2vw,2rem)] leading-tight'>
             <TextScrollAnimation
               lines={[
