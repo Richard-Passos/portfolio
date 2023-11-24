94bdbe574f9b2bd9a928875257e021d6ed1338ba
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-23 23:28:05 -0300

    Exporting new constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export { default as availabilityMessage } from './availability-message';
 export { default as pages } from './pages';
 export { default as selectedProjects } from './selected-projects';
+export { default as selectedSkills } from './selected-skills';
 export { default as socialMedias } from './socialMedias';
