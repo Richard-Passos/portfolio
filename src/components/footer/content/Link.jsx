4ce20b531ec8a2153649d5704e3f1361f5264d44
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-25 17:09:09 -0300

    Creating footer link structure

---
+++ src/components/footer/content/Link.jsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useDispatch } from 'react-redux';
+
+import { setVariant } from '@/redux';
+import { cn } from '@/utils';
+
+import { CursorLink } from '../../link';
+import { CONTENT_THEME } from '../Root';
+
+const FooterContentTitle = ({ className, ...props }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <CursorLink
+      className={cn(
+        'group w-full overflow-hidden text-[12rem] font-bold uppercase leading-none no-underline',
+        className,
+      )}
+      href='/contact'
+      onMouseLeave={() => dispatch(setVariant({ theme: CONTENT_THEME }))}
+      variant={{ theme: CONTENT_THEME }}
+      {...props}
+    >
+      <span className='relative flex animate-[scroll-x_25s_linear_infinite] whitespace-nowrap [--final-x:-100%]'>
+        <span className='ml-font-blank-space'>
+          <span className='outline-text'>CONTACT ME</span>{' '}
+          <span>• CONTACT ME •</span>
+        </span>
+
+        <span className='absolute left-full ml-font-blank-space'>
+          <span className='outline-text'>CONTACT ME</span>{' '}
+          <span>• CONTACT ME •</span>
+        </span>
+      </span>
+    </CursorLink>
+  );
+};
+
+export default FooterContentTitle;
