ad38c1b582d71e08958480c6f3ec6192a9f0e07d
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-29 14:45:44 -0300

    Changing to 2 lines and changing positioning and opacity

--- src/components/lines/Root.jsx
+++ src/components/lines/Root.jsx
@@ -1,19 +1,19 @@
 import { cn } from '@/utils';
 
-const LINES_AMOUNT = 7;
+const LINES_AMOUNT = 6;
 
 const Lines = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-around opacity-40 dark:opacity-10',
+        'pointer-events-none fixed left-1/2 top-0 -z-50 flex h-screen w-screen -translate-x-1/2 justify-center gap-4 opacity-50 dark:opacity-25',
         className,
       )}
       {...props}
     >
       {[...Array(LINES_AMOUNT)].map((_, i) => (
         <span
-          className='h-full w-px bg-border'
+          className='h-full w-full border-x'
           key={i}
         />
       ))}
