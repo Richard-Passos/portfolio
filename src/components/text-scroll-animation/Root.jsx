d587993e8176fd45dadb37c10608672ce6ef6fab
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-29 14:54:27 -0300

    Testing new animation

--- src/components/text-scroll-animation/Root.jsx
+++ src/components/text-scroll-animation/Root.jsx
@@ -1,18 +1,65 @@
+'use client';
+
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
 import { cn } from '@/utils';
 
-import Line from './Line';
+import { scrollSmoothConfig } from '../smooth-scroll';
 
-const TextScrollAnimation = ({ lines = [], className, ...props }) => {
+const TextScrollAnimation = ({ text, className, ...props }) => {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['0 .95', '0 .25'],
+  });
+
+  const smoothScroll = useSpring(scrollYProgress, scrollSmoothConfig);
+
+  const words = text.split(' ');
+
   return (
     <span
-      aria-label={lines.join(' ')}
-      className={cn('flex flex-col', className)}
+      aria-label={text}
+      className={cn('flex flex-wrap gap-x-font-blank-space', className)}
+      ref={ref}
       {...props}
     >
-      {lines.map((line, i) => (
-        <Line key={i}>{line}</Line>
-      ))}
+      {words.map((word, i) => {
+        const start = i / words.length,
+          end = start + 1 / words.length;
+
+        return (
+          <Word
+            key={i}
+            progress={smoothScroll}
+            range={[start, end]}
+          >
+            {word}
+          </Word>
+        );
+      })}
     </span>
+  );
+};
+
+const Word = ({ progress, range, className, style, ...props }) => {
+  const bgSize = useTransform(progress, range, ['0%', '100%']);
+
+  return (
+    <motion.span
+      aria-hidden
+      className={cn(
+        'bg-[linear-gradient(hsl(var(--content)),hsl(var(--content)))] bg-no-repeat [--text-opacity:.1] [-webkit-text-fill-color:hsl(var(--content)/var(--text-opacity))] [background-clip:text] [background-size:var(--bg-x-size)_100%] dark:[--text-opacity:0.05]',
+        className,
+      )}
+      style={{
+        '--bg-x-size': bgSize,
+        ...style,
+      }}
+      {...props}
+    />
   );
 };
 
