40a23824302b1675fd5fb7191d37e2858414731a
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-29 14:47:54 -0300

    Changing link name

--- src/components/list-horizontal-scroll/Link.jsx
+++ src/components/list-horizontal-scroll/Link.jsx
@@ -1,10 +1,10 @@
 import { cn } from '@/utils';
 
-import Button from '../button';
+import { MagneticButton } from '../button';
 
 const ListHorizontalScrollLink = ({ className, variants, ...props }) => {
   return (
-    <Button
+    <MagneticButton
       className={cn(
         'uppercase shadow-[0_0_50px_-15px_var(--tw-shadow-color)] shadow-[hsl(0_0%_80%/.25)] transition-[color,background-color,border-color,text-decoration-color,box-shadow,text-underline-offset] dark:shadow-[hsl(0_0%_5%/.25)]',
         className,
