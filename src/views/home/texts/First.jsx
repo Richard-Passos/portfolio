ebcaea576c73d01aac41c82aa1602fa34ada58fc
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-29 15:00:54 -0300

    Adding background with lines and a border and changing to new text animation

--- src/views/home/texts/First.jsx
+++ src/views/home/texts/First.jsx
@@ -1,4 +1,4 @@
-import { Section, TextScrollAnimation } from '@/components';
+import { Lines, Section, TextScrollAnimation } from '@/components';
 import { Text } from '@/components/ui/text';
 import { cn } from '@/utils';
 
@@ -6,24 +6,23 @@ const HomeFirstText = ({ className, ...props }) => {
   return (
     <Section
       asChild
-      className={cn('flex items-center justify-center', className)}
+      className={cn('relative flex items-center justify-center', className)}
       {...props}
     >
       <div>
-        <div className='flex w-[90%] max-w-screen-xl justify-between'>
-          <Text className='mb-auto flex -translate-y-6 items-center font-semibold'>
+        <div className='absolute inset-0 overflow-hidden bg-main transition-bg'>
+          <span className='absolute inset-x-[5%] top-0 h-px w-auto bg-content transition-bg' />
+
+          <Lines className='absolute' />
+        </div>
+
+        <div className='relative z-10 flex w-[90%] max-w-screen-xl justify-between'>
+          <Text className='-translate-y-6 font-semibold'>
             Richard Passos — developer
           </Text>
 
-          <Text className='text-[clamp(1rem,2.2vw,2rem)] leading-tight'>
-            <TextScrollAnimation
-              lines={[
-                'Helping brands achieve digital prominence.',
-                'I bring a passion for cutting-edge technology',
-                'and a commitment to transforming ideas into',
-                'impactful, user-centric solutions.',
-              ]}
-            />
+          <Text className='max-w-2xl text-[clamp(1rem,2.2vw,2rem)] leading-tight'>
+            <TextScrollAnimation text='Helping brands achieve digital prominence. I bring a passion for cutting-edge technology and a commitment to transforming ideas into impactful, user-centric solutions.' />
           </Text>
         </div>
       </div>
