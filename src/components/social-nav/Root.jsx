fe2908158b8b32feb9258d79c5ebcee9eb7f3bd7
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-29 14:54:01 -0300

    Adding icon button

--- src/components/social-nav/Root.jsx
+++ src/components/social-nav/Root.jsx
@@ -3,26 +3,28 @@ import { Link2Icon } from '@radix-ui/react-icons';
 import { socialMedias } from '@/constants';
 import { cn } from '@/utils';
 
-import { Text } from '../ui/text';
+import { IconButton } from '../button';
 import Item from './Item';
 
 const SocialNav = ({ className, ...props }) => {
   return (
     <section
       className={cn(
-        'group translate-x-4 translate-y-[4.5rem] space-y-4 p-4 transition-transform duration-300 [--icon-scale:1] [--item-opacity:0] [--item-y:100%] [--title-x:0%] focus-within:translate-y-4 focus-within:[--icon-scale:0] focus-within:[--item-opacity:1] focus-within:[--item-y:0%] focus-within:[--title-x:2.375rem] hover:translate-y-4',
+        'group translate-x-4 translate-y-[4.5rem] space-y-4 p-4 transition-transform duration-300 [--item-opacity:0] [--item-y:100%] focus-within:translate-y-4 focus-within:[--item-opacity:1] focus-within:[--item-y:0%] hover:translate-y-4',
         className,
       )}
       {...props}
     >
-      <Text className='ml-auto flex items-center gap-6 text-xs font-bold uppercase'>
-        <span className='translate-x-[--title-x] transition-transform duration-300 group-hover:translate-x-[2.375rem]'>
-          Get in touch
-        </span>{' '}
-        <span className='scale-[--icon-scale] rounded-full bg-content p-1.5 text-main transition-transform duration-300 group-hover:scale-0'>
-          <Link2Icon className='h-3.5 w-3.5' />
-        </span>{' '}
-      </Text>
+      <IconButton
+        className='pointer-events-none h-fit text-xs font-bold uppercase'
+        tabIndex={-1}
+        variants={{ color: 'inverted', style: 'transparent', size: 'sm' }}
+      >
+        Get in touch{' '}
+        <IconButton.Icon>
+          <Link2Icon />
+        </IconButton.Icon>
+      </IconButton>
 
       <nav className='flex gap-3'>
         {socialMedias.map((socialMedia, i) => (
