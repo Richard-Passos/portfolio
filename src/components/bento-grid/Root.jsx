9c95c0f4d2fc86cee1e81d79a53da330d2a39e76
Richard Passos <richard.ac.passos12@gmail.com>
2023-11-30 11:18:03 -0300

    Adding overflow-hidden to bento grid to fix mobile responsiviness

--- src/components/bento-grid/Root.jsx
+++ src/components/bento-grid/Root.jsx
@@ -4,7 +4,7 @@ const BentoGrid = ({ className, ...props }) => {
   return (
     <ul
       className={cn(
-        'grid w-full max-w-screen-lg grid-cols-1 gap-4 p-1 sm:grid-cols-2 md:w-[90%] lg:grid-cols-3',
+        'grid w-full max-w-screen-lg grid-cols-1 gap-4 overflow-hidden p-1 sm:grid-cols-2 md:w-[90%] lg:grid-cols-3',
         className,
       )}
       {...props}
