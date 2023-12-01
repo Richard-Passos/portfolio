5686f1bab80a5f32d3e87a8db0f5f950ff7e9adc
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-01 17:07:33 -0300

    Increasing touch multiplier to 2

--- src/components/smooth-scroll/Root.jsx
+++ src/components/smooth-scroll/Root.jsx
@@ -5,7 +5,7 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 const SmoothScroll = (props) => {
   return (
     <ReactLenis
-      options={{ smoothTouch: true, touchMultiplier: 0.5 }}
+      options={{ smoothTouch: true, touchMultiplier: 2 }}
       root
       {...props}
     />
