14ac25d2cbbc761029a9dbf2589f6aecddd88029
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-05 21:23:11 -0300

    Making it a flex container

--- src/components/social-nav/Root.jsx
+++ src/components/social-nav/Root.jsx
@@ -10,13 +10,13 @@ const SocialNav = ({ className, ...props }) => {
   return (
     <section
       className={cn(
-        'group translate-x-4 translate-y-[4.5rem] space-y-4 p-4 transition-transform duration-300 [--item-opacity:0] [--item-y:100%] focus-within:translate-y-4 focus-within:[--item-opacity:1] focus-within:[--item-y:0%] hover:translate-y-4',
+        'group flex translate-x-4 translate-y-[4.5rem] flex-col items-end gap-4 p-4 transition-transform duration-300 [--item-opacity:0] [--item-y:100%] focus-within:translate-y-4 focus-within:[--item-opacity:1] focus-within:[--item-y:0%] hover:translate-y-4',
         className,
       )}
       {...props}
     >
       <IconButton
-        className='pointer-events-none h-fit text-xs font-bold uppercase'
+        className='pointer-events-none ml-auto h-fit text-xs font-bold uppercase'
         tabIndex={-1}
         variants={{ color: 'inverted', style: 'transparent', size: 'sm' }}
       >
