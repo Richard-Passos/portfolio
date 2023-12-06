2d7306f747400168820a29b25feec20ca3dc8f35
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-05 21:29:19 -0300

    Testing design

--- src/components/social-nav/Item.jsx
+++ src/components/social-nav/Item.jsx
@@ -6,14 +6,14 @@ const SocialNavItem = ({ className, index, href, name, ...props }) => {
   return (
     <div
       className={cn(
-        'aspect-square h-10 translate-y-[--item-y] rounded-sm text-sm opacity-[--item-opacity] transition-all duration-300 ease-backOut group-hover:translate-y-0 group-hover:opacity-100 group-hover:[transition-delay:--delay]',
+        'h-10 translate-y-[--item-y] rounded-sm text-sm opacity-[--item-opacity] transition-all duration-300 ease-backOut group-hover:translate-y-0 group-hover:opacity-100 group-hover:[transition-delay:--delay]',
         className,
       )}
       style={{ '--delay': 0.1 * index + 's' }}
       {...props}
     >
       <MagneticLink
-        className='h-full w-full rounded-inherit'
+        git
         href={href}
       >
         {capitalize(name)}
