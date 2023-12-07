c92f2e31c67fb7fa0885bab88701ad061a4036ad
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-07 20:55:41 -0300

    Reduxing text size adding font-semibild

--- src/views/home/texts/Second.jsx
+++ src/views/home/texts/Second.jsx
@@ -10,7 +10,7 @@ const HomeSecondText = ({ className, ...props }) => {
       {...props}
     >
       <div>
-        <Text className='w-[90%] text-4xl leading-tight md:text-5xl md:leading-tight'>
+        <Text className='w-[90%] max-w-screen-lg text-3xl font-semibold leading-tight md:text-4xl'>
           <TextScrollAnimation
             className='mx-auto justify-center'
             text="If you're looking for a developer who's got the skills, the passion, and a problem solving mindset to make your ideas come true. Then..."
