a06320286be8080c3d13e5eb7d0bf5301dbb0fe1
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-07 19:32:33 -0300

    Adding focus-visible:outline-variant-content

--- src/components/list-horizontal-scroll/Link.jsx
+++ src/components/list-horizontal-scroll/Link.jsx
@@ -6,12 +6,12 @@ const ListHorizontalScrollLink = ({ className, variants, ...props }) => {
   return (
     <MagneticButton
       className={cn(
-        'uppercase shadow-[0_0_50px_-15px_var(--tw-shadow-color)] shadow-[hsl(0_0%_80%/.25)] transition-[color,background-color,border-color,text-decoration-color,box-shadow,text-underline-offset] dark:shadow-[hsl(0_0%_5%/.25)]',
+        'uppercase shadow-[0_0_50px_-15px_var(--tw-shadow-color)] shadow-[hsl(0_0%_80%/.25)] transition-[color,background-color,border-color,text-decoration-color,box-shadow,text-underline-offset] focus-visible:outline-variant-content dark:shadow-[hsl(0_0%_5%/.25)]',
         className,
       )}
       isLink
       limit={0.4}
-      variants={{ color: 'main', style: 'basic', size: 'lg', ...variants }}
+      variants={{ color: 'main', size: 'lg', ...variants }}
       {...props}
     />
   );
