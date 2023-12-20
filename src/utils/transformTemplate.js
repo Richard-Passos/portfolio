7ac204ec62eefdceb3a5296e648e91ff7c4ffbe0
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 16:31:12 -0300

    Refactoring and adding new rotate

--- src/utils/transformTemplate.js
+++ src/utils/transformTemplate.js
@@ -1,9 +1,21 @@
-const transformTemplate = ({ x, y, rotate, scale, scaleX, scaleY }) =>
-  `translate(${x ?? 'var(--tw-translate-x)'}, ${
-    y ?? 'var(--tw-translate-y)'
-  }) rotate(${rotate ?? 'var(--tw-rotate)'}) scale(${
-    scale ??
-    `${scaleX ?? 'var(--tw-scale-x)'}, ${scaleY ?? 'var(--tw-scale-y)'}`
-  })`;
+const transformTemplate = (transform) => {
+  const translate = `translate(${transform.x ?? 'var(--tw-translate-x)'}, ${
+      transform.y ?? 'var(--tw-translate-y)'
+    })`,
+    rotate =
+      transform.rotateX || transform.rotateY
+        ? `rotateX(${
+            transform.rotateX ?? 'var(--tw-rotate-x, 0deg)'
+          }) rotateY(${transform.rotateY ?? 'var(--tw-rotate-y, 0deg)'})`
+        : `rotate(${transform.rotate ?? 'var(--tw-rotate)'})`,
+    scale = `scale(${
+      transform.scale ??
+      `${transform.scaleX ?? 'var(--tw-scale-x)'}, ${
+        transform.scaleY ?? 'var(--tw-scale-y)'
+      }`
+    })`;
+
+  return `${translate} ${rotate} ${scale}`;
+};
 
 export default transformTemplate;
