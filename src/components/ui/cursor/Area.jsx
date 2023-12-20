1d3537d7aa84f1233bce97e68738c9f2aff4e13e
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 16:38:43 -0300

    Adding cursor-content id

--- src/components/ui/cursor/Area.jsx
+++ src/components/ui/cursor/Area.jsx
@@ -1,12 +1,13 @@
 import { forwardRef } from 'react';
 
 import { FollowPointerArea } from '../follow-pointer';
+import { CURSOR_CONTENT_ID } from './Content';
 import { CURSOR_ID } from './Root';
 
 const CursorArea = (props, ref) => {
   return (
     <FollowPointerArea
-      id={CURSOR_ID}
+      id={[CURSOR_ID, CURSOR_CONTENT_ID]}
       ref={ref}
       {...props}
     />
