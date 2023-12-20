3e08210e63d0e1db7f97d1f5dec9c24e56c611f3
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 16:32:50 -0300

    Exporting new contexts

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { default as FormFieldContext } from './FormField';
-export { default as ListProjectsContext } from './ListProjects';
 export { default as ProgressContext } from './Progress';
+export { default as ProjectsContext } from './Projects';
+export { default as ProjectsShowContext } from './ProjectsShow';
