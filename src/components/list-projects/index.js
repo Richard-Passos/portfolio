b3a6bf27961f603b178b5dfeffe5d36d0f921beb
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 16:48:49 -0300

    Renaming list-projects

--- src/components/list-projects/index.js
+++
@@ -1,9 +0,0 @@
-import ListProjectsItem from './Item';
-import ListProjectsRoot from './Root';
-
-const ListProjects = ListProjectsRoot;
-
-ListProjects.Item = ListProjectsItem;
-
-export default ListProjects;
-export { ListProjectsRoot as ListProjects, ListProjectsItem };
