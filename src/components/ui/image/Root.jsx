89b3346675c64c8230b30f8e684f1ce2b3030048
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 16:36:10 -0300

    Refactoring

--- src/components/ui/image/Root.jsx
+++ src/components/ui/image/Root.jsx
@@ -1,26 +1,25 @@
 import NextImage from 'next/image';
 import { forwardRef } from 'react';
 
-const Image = ({ src, alt, fill, ...props }, ref) => {
+const Image = ({ src, alt, ...props }, ref) => {
   const placeholderProps = {
     src: '/images/placeholder.png',
     alt: 'A black and white image with the word image on it.',
-    ...(!fill && { width: 556, height: 350 }),
+    ...(!props.fill && { width: 556, height: 350 }),
   };
 
-  props = {
-    ref,
-    fill,
-    ...props,
-    ...(src
-      ? {
-          src,
-          alt: alt || 'Missing alt property.',
-        }
-      : placeholderProps),
-  };
-
-  return <NextImage {...props} />;
+  return (
+    <NextImage
+      ref={ref}
+      {...(src
+        ? {
+            src,
+            alt: alt || 'Missing alt property.',
+          }
+        : placeholderProps)}
+      {...props}
+    />
+  );
 };
 
 export default forwardRef(Image);
