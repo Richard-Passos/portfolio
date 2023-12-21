e9903baf33820a87b19081810616ee4ebdff6716
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-21 20:53:20 -0300

    Changing h-20 to h-28

--- src/components/ui/textarea/Root.jsx
+++ src/components/ui/textarea/Root.jsx
@@ -6,7 +6,7 @@ const Textarea = ({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        'h-20 w-full resize-none rounded-sm border bg-main px-4 py-3 text-sm placeholder:text-muted-content disabled:cursor-not-allowed disabled:opacity-50',
+        'h-28 w-full resize-none rounded-sm border bg-main px-4 py-3 text-sm placeholder:text-muted-content disabled:cursor-not-allowed disabled:opacity-50',
         className,
       )}
       ref={ref}
