d9b0f0997f94ce743a70caec317118ce66f6b47a
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-20 21:28:36 -0300

    Creating useGetNumberOfSiblings structure

---
+++ src/hooks/useGetNumberOfSiblings.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const useGetNumberOfSiblings = (
+  containerRef,
+  childrenRef,
+  containerTimes = 2,
+  returnJustOddNumber = false,
+) => {
+  const [numberOfSiblings, setNumberOfSiblings] = useState(1);
+
+  useEffect(() => {
+    const handleSetNumberOfSiblings = () => {
+      if (containerRef.current && childrenRef.current) {
+        const containerWidth =
+            containerRef.current.getBoundingClientRect().width,
+          childrenWidht = childrenRef.current.getBoundingClientRect().width;
+
+        const numberOfChildren = Math.round(
+          (containerWidth * containerTimes) / childrenWidht,
+        );
+
+        setNumberOfSiblings(
+          Math.max(
+            returnJustOddNumber && numberOfChildren % 2 !== 0
+              ? numberOfChildren
+              : numberOfChildren - 1,
+            1,
+          ),
+        );
+      }
+    };
+
+    handleSetNumberOfSiblings();
+
+    window.addEventListener('resize', handleSetNumberOfSiblings);
+
+    return () =>
+      window.removeEventListener('resize', handleSetNumberOfSiblings);
+  }, [containerRef, childrenRef, containerTimes, returnJustOddNumber]);
+
+  return numberOfSiblings;
+};
+
+export default useGetNumberOfSiblings;
