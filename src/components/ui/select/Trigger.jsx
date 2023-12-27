eabfc58f32a199817df93e4284a52048802685fd
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-26 22:03:13 -0300

    Changing from h-10 to h-14

--- src/components/ui/select/Trigger.jsx
+++ src/components/ui/select/Trigger.jsx
@@ -9,7 +9,7 @@ const SelectTrigger = ({ className, ...props }, ref) => {
   return (
     <Trigger
       className={cn(
-        'group flex h-10 w-full items-center justify-between rounded-sm border bg-main px-4 text-sm focus:outline disabled:cursor-not-allowed disabled:opacity-50 data-open:outline [&>:not(svg)]:data-[placeholder]:text-muted-content',
+        'group flex h-14 w-full items-center justify-between rounded-sm border bg-main px-4 text-sm focus:outline disabled:cursor-not-allowed disabled:opacity-50 data-open:outline [&>:not(svg)]:data-[placeholder]:text-muted-content',
         className,
       )}
       ref={ref}
