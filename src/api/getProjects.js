3b94a5c2bb12a210049b92b92ed2bc66d515cdcd
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-27 09:25:23 -0300

    Debugging on deploy

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -4,6 +4,7 @@ const getProjects = async (params = '', init = {}) => {
   const res = await fetch(`${baseUrl}/api/projects?${params}`, init);
 
   const data = await res.json();
+  console.log('-  data   -', data);
 
   return data;
 };
--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -7,12 +7,14 @@ const RES_PER_PAGE = 5;
 const GET = async (req) => {
   try {
     const page = req.nextUrl.searchParams.get('page') || 1;
+    console.log('-  page   -', page)
 
     const results = projects.slice(
         (page - 1) * RES_PER_PAGE,
         page * RES_PER_PAGE,
       ),
       totalResults = projects.length;
+      console.log('-  results   -', results)
 
     return NextResponse.json({
       status: 200,
