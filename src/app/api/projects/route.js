248a740697a7fbb0aa2609b80fa6f7923ec25e85
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-27 09:57:35 -0300

    Debugging on deploy

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -25,6 +25,7 @@ const GET = async (req) => {
       results,
       totalPages: Math.ceil(totalResults / RES_PER_PAGE),
       totalResults,
+      log,
     });
   } catch {
     return NextResponse.json({
