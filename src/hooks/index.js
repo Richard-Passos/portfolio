39e29da91c07d561aceae8c960ceec533afd9f67
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-26 22:01:39 -0300

    Exporting new hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,6 +3,8 @@ export { default as useFollowPointer } from './useFollowPointer';
 export { default as useFormField } from './useFormField';
 export { default as useGetNumberOfSiblings } from './useGetNumberOfSiblings';
 export { default as useIsExternalUrl } from './useIsExternalUrl';
+export { default as useIsInView } from './useIsInView';
+export { default as useLoadMore } from './useLoadMore';
 export { default as useLocalTime } from './useLocalTime';
 export { default as useSmooth } from './useSmooth';
 export { default as useToast } from './useToast';
