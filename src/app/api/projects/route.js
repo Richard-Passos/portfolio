5e8e70a5345bdbb2bf6bcc2d92df02f7218e81a0
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-27 18:19:37 -0300

    Debugging on production

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -5,31 +5,23 @@ import { projects } from '@/constants';
 const RES_PER_PAGE = 5;
 
 const GET = async (req) => {
-  try {
-    const { searchParams } = new URL(req.url);
+  const { searchParams } = new URL(req.url);
 
-    const page = searchParams.get('page') || 1;
+  const page = searchParams.get('page') || 1;
 
-    const results = projects.slice(
-        (page - 1) * RES_PER_PAGE,
-        page * RES_PER_PAGE,
-      ),
-      totalResults = projects.length;
+  const results = projects.slice(
+      (page - 1) * RES_PER_PAGE,
+      page * RES_PER_PAGE,
+    ),
+    totalResults = projects.length;
 
-    return NextResponse.json({
-      status: 200,
-      page,
-      results,
-      totalPages: Math.ceil(totalResults / RES_PER_PAGE),
-      totalResults,
-    });
-  } catch {
-    return NextResponse.json({
-      status: 400,
-      message: 'Failed to fetch data.',
-      results: [],
-    });
-  }
+  return NextResponse.json({
+    status: 200,
+    page,
+    results,
+    totalPages: Math.ceil(totalResults / RES_PER_PAGE),
+    totalResults,
+  });
 };
 
 export { GET };
