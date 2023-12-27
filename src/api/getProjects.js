5f2faf0bee0da166758edcd579c9c2bf3cdca8ab
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-26 22:19:07 -0300

    Trying to fix deploy bug

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -1,7 +1,10 @@
 import { baseUrl } from '@/constants';
 
 const getProjects = async (params = '', init = {}) => {
-  const res = await fetch(`${baseUrl}/api/projects?${params}`, init);
+  const res = await fetch(`${baseUrl}/api/projects?${params}`, {
+    'Content-Type': 'application/json',
+    ...init,
+  });
 
   const data = await res.json();
 
