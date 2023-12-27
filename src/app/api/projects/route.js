9d65b54f5c6b262a94d1a423ee5bf7c94553571b
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-27 16:46:37 -0300

    Debugging on production

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -22,6 +22,7 @@ const GET = async (req) => {
       results,
       totalPages: Math.ceil(totalResults / RES_PER_PAGE),
       totalResults,
+      log: '',
     });
   } catch {
     return NextResponse.json({
