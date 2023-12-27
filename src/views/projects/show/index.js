db901b22db9d6341a36ae7d3d378d7c905d00b33
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-26 21:54:09 -0300

    Exporting new component

--- src/views/projects/show/index.js
+++ src/views/projects/show/index.js
@@ -1,18 +1,21 @@
-import ProjectsShowRoot from './Root';
 import ProjectsShowContent from './Content';
-import ProjectsShowTypes from './Types';
+import ProjectsShowLoadMore from './LoadMore';
 import ProjectsShowRoles from './Roles';
+import ProjectsShowRoot from './Root';
+import ProjectsShowTypes from './Types';
 
-const ProjectsShow = ProjectsShowRoot
+const ProjectsShow = ProjectsShowRoot;
 
 ProjectsShow.Content = ProjectsShowContent;
+ProjectsShow.LoadMore = ProjectsShowLoadMore;
 ProjectsShow.Roles = ProjectsShowRoles;
 ProjectsShow.Types = ProjectsShowTypes;
 
 export default ProjectsShow;
 export {
-ProjectsShowRoot as ProjectsShow,
-ProjectsShowContent,
-ProjectsShowRoles,
-ProjectsShowTypes
-}
+  ProjectsShowRoot as ProjectsShow,
+  ProjectsShowContent,
+  ProjectsShowLoadMore,
+  ProjectsShowRoles,
+  ProjectsShowTypes,
+};
