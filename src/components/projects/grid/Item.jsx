6851c2d645e0926b8e4e6e38ab8c403e4f128ef5
Richard Passos <richard.ac.passos12@gmail.com>
2023-12-26 22:05:40 -0300

    Testing design

--- src/components/projects/grid/Item.jsx
+++ src/components/projects/grid/Item.jsx
@@ -6,83 +6,77 @@ import { ProjectsContext } from '@/contexts';
 import { cn } from '@/utils';
 
 import { CursorLink } from '../../link';
-import ScrollAnimation from '../../scroll-animation';
+import { ScrollAnimationTransform } from '../../scroll-animation';
 import { Badge, Image } from '../../ui';
-import Title from './Title';
+import Title from '../Title';
 
-const ANIMATION_OFFSET = ['-.25 1', '0 1'];
+const ANIMATION_OFFSET = ['0 1', '.25 1'];
 
-const ProjectsGridItem = ({ index, className, style, data, ...props }) => {
+const ProjectsGridItem = ({ index, className, data, ...props }) => {
   const { images } = useContext(ProjectsContext);
 
   const img = images[index];
 
   const animationConfig = {
-    scale: {
-      useScrollConfig: { offset: ANIMATION_OFFSET },
-      prop: 'scale',
-      scrollPoints: [0, 0.999, 1],
-      propPoints: [0.95, 0.95, 1],
-    },
-    clipPath: {
-      useScrollConfig: { offset: ANIMATION_OFFSET },
-      useScrollRes: 'scrollYProgress',
-      prop: '--clip-path',
-      scrollPoints: [0, 0.999, 1],
-      propPoints: [1, 1, 0],
-    },
+    useScrollConfig: { offset: ANIMATION_OFFSET },
+    prop: '--y',
+    propPoints: [0.25, 0],
   };
 
   return (
     <li
       className={cn(
-        'w-full space-y-6 sm:w-[calc(50%_-_var(--gap)/2)] sm:even:ml-auto sm:first:[&:not(:first-child)>*]:-mt-[45%]',
+        'w-full sm:w-[calc(50%_-_var(--gap)/2)] sm:even:ml-auto sm:[&:not(:first-child)>div]:-mt-[60%]',
         className,
       )}
-      style={{
-        '--idx': index,
-        ...style,
-      }}
       {...props}
     >
-      <ScrollAnimation.Transform
-        config={animationConfig.scale}
-        smoothConfig={{ stiffness: 50 }}
+      <ScrollAnimationTransform
+        config={animationConfig}
+        smoothConfig={{ stiffness: 75 }}
       >
-        <CursorLink
-          className='group relative aspect-square w-full overflow-hidden rounded-lg bg-muted'
-          href={data.href}
-        >
-          <ScrollAnimation
-            config={animationConfig.clipPath}
-            smoothConfig={{ stiffness: 50 }}
+        <div className='translate-y-[calc(var(--y)*100%)] space-y-6'>
+          <CursorLink
+            className='group relative aspect-[1/1.25] w-full overflow-hidden rounded-lg'
+            content={{
+              name: 'EyeOpen',
+            }}
+            href={data.href}
           >
-            {/* eslint-disable-next-line jsx-a11y/alt-text */}
-            <Image
-              className='h-full w-full object-cover transition-transform duration-300 [clip-path:inset(0_0_calc(var(--clip-path)*100%)_0)] group-hover:scale-105'
-              {...img}
+            <div className='relative h-full w-full overflow-hidden rounded-inherit border border-transparent transition-transform duration-300 group-hover:scale-[.98]'>
+              {/* eslint-disable-next-line jsx-a11y/alt-text */}
+              <Image
+                className='h-full w-full object-cover'
+                {...img}
+              />
+
+              <span className='absolute inset-0 -z-10 bg-muted transition-bg' />
+            </div>
+
+            <span className='absolute inset-0 bg-main/75 opacity-0 backdrop-blur-sm transition-[background-color,opacity] group-hover:opacity-100' />
+
+            <Title
+              animationType='onGroupHover'
+              className='absolute text-[9vw] sm:text-[min(4vw,2.25rem)]'
+              text={data.title}
             />
-          </ScrollAnimation>
+          </CursorLink>
 
-          <span className='pointer-events-none absolute inset-0 bg-main/75 opacity-0 backdrop-blur-sm transition-opacity group-hover:opacity-100' />
-
-          <Title text={data.title} />
-        </CursorLink>
-      </ScrollAnimation.Transform>
-
-      <div className='flex flex-wrap gap-1.5'>
-        {data.roles
-          .sort((a, b) => a.localeCompare(b))
-          .map((role) => (
-            <Badge
-              className='lowercase [--variant:--muted-c]'
-              key={role}
-              variants={{ style: 'outline' }}
-            >
-              {role}
-            </Badge>
-          ))}
-      </div>
+          <div className='flex flex-wrap gap-1.5'>
+            {data.roles
+              .sort((a, b) => a.localeCompare(b))
+              .map((role) => (
+                <Badge
+                  className='lowercase [--variant:--muted-c]'
+                  key={role}
+                  variants={{ style: 'outline' }}
+                >
+                  {role}
+                </Badge>
+              ))}
+          </div>
+        </div>
+      </ScrollAnimationTransform>
     </li>
   );
 };
