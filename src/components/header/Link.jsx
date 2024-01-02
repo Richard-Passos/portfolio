8f9c1f53f0b8196655d968ba27c70d6d62798ffa
Richard Passos <richard.ac.passos12@gmail.com>
2024-01-02 20:12:00 -0300

    Testing design

--- src/components/header/Link.jsx
+++ src/components/header/Link.jsx
@@ -6,26 +6,27 @@ import { smoothConfig } from '@/hooks/useSmooth';
 import { cn } from '@/utils';
 
 import { MagneticLink } from '../link';
-import Magnetic from '../magnetic';
 import { NavigationMenuLink } from '../ui/navigation-menu';
 
 const HeaderLink = ({ isActive, className, children, ...props }) => {
   return (
     <MagneticLink
       asChild
-      className={cn('relative h-10 rounded-sm', isActive && '-z-10', className)}
+      className={cn(
+        'relative h-10 rounded-sm px-4 no-underline hover:z-10',
+        isActive && '-z-10',
+        className,
+      )}
       {...props}
     >
       <NavigationMenuLink>
-        <Magnetic limit={0.15}>
-          <span className='flex h-full items-center rounded-inherit px-4'>
-            {children}
-          </span>
-        </Magnetic>
+        {children}
 
         {isActive && (
           <motion.span
-            className={cn('absolute inset-0 -z-10 rounded-inherit bg-muted')}
+            className={cn(
+              'absolute inset-x-4 bottom-1.5 h-px bg-current transition-bg',
+            )}
             layoutId='headerLinkActiveIndicator'
             transition={{
               type: 'spring',
