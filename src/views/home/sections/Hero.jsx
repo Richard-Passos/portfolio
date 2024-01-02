0acbd38e3835e36552d9aeb575fb3440b84e2e53
Richard Passos <richard.ac.passos12@gmail.com>
2024-01-02 20:00:04 -0300

    Testing new design

--- src/views/home/sections/Hero.jsx
+++ src/views/home/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import { InfinityScroll, Section, TextRing } from '@/components';
+import { ScrollIndicator, Section } from '@/components';
 import { ScrollAnimationTransform } from '@/components/scroll-animation';
 import { Text } from '@/components/ui';
 import { cn } from '@/utils';
@@ -12,119 +12,75 @@ const HomeHeroSection = ({ className, ...props }) => {
         offset: ANIMATION_OFFSET,
       },
       prop: '--x',
-      propPoints: ['0vw', '-10vw'],
+      propPoints: [0, 1],
     },
-    rotate: {
+    y: {
       useScrollConfig: {
         offset: ANIMATION_OFFSET,
       },
-      prop: '--rotate',
-      propPoints: ['0deg', '-360deg'],
+      propPoints: ['0%', '100%'],
     },
   };
 
   return (
     <ScrollAnimationTransform config={animationConfig.x}>
-      <ScrollAnimationTransform config={animationConfig.rotate}>
-        <Section
-          className={cn(
-            'relative flex flex-col items-center justify-center gap-4 pb-14 pt-28',
-            className,
-          )}
-          {...props}
-        >
-          <div className='flex w-[90%] max-w-screen-xl flex-col items-center gap-4 pb-32'>
-            <Title
-              asChild
-              className='flex w-full flex-col'
-            >
-              <h1>
-                <span className='translate-x-[--x]'>Richard P.</span>
-
-                <span className='flex w-full items-center justify-between gap-font-blank-space'>
-                  <span
-                    aria-hidden
-                    className='flex max-w-xs flex-col text-base font-semibold normal-case text-muted-content max-lg:hidden'
-                  >
-                    <span className='ml-[2em]'>
-                      that&apos;s what I am — solid and
-                    </span>
-                    <span>scalable products with a great user</span>
-                    <span>experience, that&apos;s what I build.</span>
-                  </span>
-
-                  <span className='shrink-0 translate-x-[calc(var(--x)*-1)]'>
-                    full stack
-                  </span>
-                </span>
-
-                <span className='flex translate-x-[--x] items-center gap-[calc(var(--font-blank-space)*3)]'>
-                  <span>developer</span>
-
-                  <span
-                    aria-hidden
-                    className='relative flex items-center justify-center text-[.15em] text-content max-sm:hidden'
-                  >
-                    ✦{' '}
-                    <TextRing
-                      className='rotate-[--rotate] animate-none text-[.65em]'
-                      text='made with ❤ made with ❤ '
-                    />
-                  </span>
-                </span>
-              </h1>
-            </Title>
-
-            <span className='max-w-xs text-center font-semibold text-muted-content lg:sr-only'>
-              that&apos;s what I am — solid and scalable products with a great
-              user experience, that&apos;s what I build.
-            </span>
-          </div>
-
-          <div className='absolute inset-0 bottom-0 -z-10 w-auto bg-main transition-bg md:bottom-[5%]'>
-            <span className='absolute bottom-0 left-1/2 h-px w-[90%] max-w-screen-xl -translate-x-1/2 bg-border opacity-50 transition-bg dark:opacity-20' />
-          </div>
-        </Section>
-      </ScrollAnimationTransform>
-    </ScrollAnimationTransform>
-  );
-};
-
-const Title = ({ className, ...props }) => {
-  return (
-    <Text.Title
-      className={cn(
-        'text-[14vw] uppercase leading-none sm:text-[min(10vw,8.5rem)]',
-        className,
-      )}
-      {...props}
-    />
-  );
-};
-
-const List = ({ className, content, ...props }) => {
-  return (
-    <div
-      className={cn(
-        'w-full max-w-md overflow-hidden rounded-full text-[clamp(1rem,7.5vw,6rem)]',
-        className,
-      )}
-      {...props}
-    >
-      <InfinityScroll
-        as='ul'
-        className='bg-content py-4 text-main transition-colors [--duration:25s] [--gap:--font-blank-space] hover:paused'
+      <Section
+        className={cn(
+          'flex flex-col items-center pb-[calc(var(--padding)*2)] pt-[--padding] [--padding:min(28vh,theme(spacing.28))]',
+          className,
+        )}
+        {...props}
       >
-        {content.map((content, i) => (
-          <li
-            key={i}
-            role={i % 2 === 0 ? 'separator' : 'listitem'}
+        <div className='relative flex w-[90%] max-w-screen-xl flex-col gap-sm max-lg:items-center'>
+          <Text.Title
+            asChild
+            className='w-full text-center text-[13vw]/[1] font-bold uppercase sm:text-[min(10vw,7rem)]/[1]'
           >
-            {content}
-          </li>
-        ))}
-      </InfinityScroll>
-    </div>
+            <h1>
+              <span className='lg:-translate-x-[min(20vw,theme(spacing.20))]'>
+                Turning heads
+              </span>
+              <br />
+              <span className='lg:translate-x-[min(20vw,theme(spacing.20))]'>
+                <span className='outline-text'>&</span> conquering
+              </span>
+              <br />
+              <div className='flex w-full items-end justify-evenly'>
+                <span>hearts</span>
+
+                <span
+                  aria-hidden
+                  className='max-w-sm -translate-y-3 text-start text-[.14em]/[1.15] font-medium normal-case max-lg:hidden'
+                >
+                  Hey there! I&apos;m Richard an awesome full stack developer —
+                  who cares building solid and scalable products with a great
+                  user experience.
+                </span>
+              </div>
+            </h1>
+          </Text.Title>
+
+          <Text className='max-w-lg text-center font-medium lg:sr-only'>
+            Hey there! I&apos;m Richard an awesome full stack developer — who
+            cares building solid and scalable products with a great user
+            experience.
+          </Text>
+        </div>
+
+        <div className='pointer-events-none absolute inset-0 -z-50 -translate-y-36 overflow-hidden'>
+          <ScrollIndicator
+            className='pointer-events-auto absolute bottom-12 right-12 z-10'
+            href='#scrollTo'
+          />
+
+          <div className='absolute inset-0 bg-main transition-bg'>
+            <span className='absolute inset-0 bg-[linear-gradient(theme(colors.border)_1px,_transparent_1px),_linear-gradient(to_right,_theme(colors.border)_1px,__transparent_1px_1px)] bg-center opacity-50 [background-size:25px_25px] [mask-image:radial-gradient(#000,transparent_75%)] dark:opacity-20' />
+
+            <span className='absolute bottom-0 h-px w-full bg-border opacity-50 transition-bg dark:opacity-20' />
+          </div>
+        </div>
+      </Section>
+    </ScrollAnimationTransform>
   );
 };
 
