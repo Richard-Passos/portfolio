4d4f577cb8ebaadc227f81898d5dd423a2cedc44
Richard Passos <richard.ac.passos12@gmail.com>
2024-01-29 10:24:55 -0300

    Creating texts

---
+++ src/constants/texts/index.js
@@ -0,0 +1,10 @@
+import aboutText from './about';
+import introText from './intro';
+
+const texts = {
+  about: aboutText,
+  intro: introText,
+};
+
+export default texts;
+export { aboutText, introText };
---
+++ src/constants/texts/intro.js
@@ -0,0 +1,7 @@
+import personalInfo from '../personalInfo';
+
+const introText = `Hey there! I'm ${
+  personalInfo.name.first
+} an awesome ${personalInfo.job.toLowerCase()} — who cares building solid and scalable products with a great user experience.`;
+
+export default introText;
