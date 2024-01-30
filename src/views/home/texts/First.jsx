8d03d4df685e92e59108fabbdbe8504abbf8cae7
Richard Passos <richard.ac.passos12@gmail.com>
2024-01-30 20:04:00 -0300

    Fixing animation

--- src/views/home/texts/First.jsx
+++ src/views/home/texts/First.jsx
@@ -5,76 +5,73 @@ import {
   TextScrollAnimation,
 } from '@/components';
 import { Text } from '@/components/ui/text';
-import { cn } from '@/utils';
 
-const HomeFirstText = ({ className, ...props }) => {
+const HomeFirstText = (props) => {
   const animationConfig = {
-    y1: {
+    x: {
       useScrollConfig: {
-        offset: ['1 1', '1.5 1'],
+         ['-.5 .5', '0 .5'],
       },
-      propPoints: ['0%', '50%'],
-    },
-    y2: {
-      useScrollConfig: {
-        offset: ['0 1', '1 1'],
-      },
-      propPoints: ['0%', '-50%'],
+      prop: '--x',
+      propPoints: ['-100%', '0%'],
     },
     clipPath: {
       useScrollConfig: {
-        offset: ['.75 1', '1.1 1'],
+         ['0 .5', '.5 .5'],
       },
       useScrollRes: 'scrollYProgress',
       prop: '--clip-path',
       scrollPoints: [0, 1],
       propPoints: ['inset(0% 0 0 0)', 'inset(100% 0 0 0)'],
     },
-    x: {
+    y: {
       useScrollConfig: {
-        offset: ['0 1', '.75 1'],
+         ['.5 .5', '1 .5'],
       },
-      prop: 'x',
-      propPoints: ['-100%', '0%'],
+      propPoints: ['0%', '50%'],
+    },
+    top: {
+      useScrollConfig: {
+         ['-1 .5', '0 .5'],
+      },
+      useScrollRes: 'scrollYProgress',
+      prop: '--top',
+      scrollPoints: [0, 1],
+      propPoints: ['0%', '-50%'],
     },
   };
 
   return (
     <Section
       as='div'
-      className={cn(
-        'relative flex h-[150vh] flex-col items-center 2xl:max-h-[calc(var(--max-h)*1.5)]',
-        className,
-      )}
       {...props}
     >
-      <ScrollAnimation.Transform
-        className='flex h-[66.666%] items-center overflow-x-clip'
-        config={animationConfig.y1}
-      >
-        <ScrollAnimation config={animationConfig.clipPath}>
-          <div>
-            <ScrollAnimation.Transform config={animationConfig.x}>
-              <Text
-                className='outline-text relative text-[clamp(1rem,44vw,24rem)] font-bold before:pointer-events-none before:absolute before:origin-bottom before:overflow-hidden before:content-[attr(data-text)] before:[-webkit-text-fill-color:currentColor] before:[clip-path:--clip-path]'
-                data-text='BUT'
-              >
-                BUT
-              </Text>
+      <div className='grid grid-rows-3 justify-items-center [--gap:--gap-md] max-2xl:min-h-[150vh] 2xl:min-h-[calc(var(--max-h)*1.5)]'>
+        <ScrollAnimation.Transform config={animationConfig.x}>
+          <ScrollAnimation config={animationConfig.clipPath}>
+            <ScrollAnimation.Transform config={animationConfig.y}>
+              <div className='row-span-2 flex items-center overflow-hidden py-[--gap]'>
+                <Text
+                  className='outline-text relative translate-x-[--x] text-[clamp(8rem,44vw,24rem)]/[1] font-bold before:pointer-events-none before:absolute before:overflow-hidden before:content-[attr(data-text)] before:[-webkit-text-fill-color:currentColor] before:[clip-path:--clip-path]'
+                  data-text='BUT'
+                >
+                  BUT
+                </Text>
+              </div>
             </ScrollAnimation.Transform>
+          </ScrollAnimation>
+        </ScrollAnimation.Transform>
+
+        <ScrollAnimation config={animationConfig.top}>
+          <div className='flex w-[90%] items-center justify-center'>
+            <Text className='relative top-[--top] text-[clamp(2rem,6.6vw,6rem)] font-semibold leading-tight'>
+              <TextScrollAnimation
+                className='justify-center'
+                text="it's not just about the tech."
+              />
+            </Text>
           </div>
         </ScrollAnimation>
-      </ScrollAnimation.Transform>
-
-      <div className='h-[33.333%] w-[90%]'>
-        <ScrollAnimation.Transform config={animationConfig.y2}>
-          <Text className='mx-auto text-[clamp(2rem,6.6vw,6rem)] font-bold leading-tight'>
-            <TextScrollAnimation
-              className='justify-center'
-              text="it's not just about the tech."
-            />
-          </Text>
-        </ScrollAnimation.Transform>
       </div>
 
       <Lines />
