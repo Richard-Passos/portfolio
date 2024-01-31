979f5ecb314ddda11f39513656ba846eab7f3566
Richard Passos <richard.ac.passos12@gmail.com>
2024-01-31 18:14:08 -0300

    Testing design

--- src/components/social-nav/Item.jsx
+++ src/components/social-nav/Item.jsx
@@ -8,26 +8,24 @@ const SocialNavItem = ({ className, icon, name, ...props }) => {
   return (
     <MagneticLink
       className={cn(
-        'group h-24 w-full flex-col items-stretch justify-between gap-3.5 rounded-sm border bg-main p-3.5 no-underline hover:bg-inverted hover:text-inverted-content sm:aspect-square',
+        'hover:dark group h-full w-full flex-col items-start justify-start gap-1.5 rounded-sm border bg-main p-3.5 no-underline sm:aspect-square',
         className,
       )}
       limit={0.15}
       {...props}
     >
-      <div className='flex flex-wrap items-center justify-between gap-3.5'>
-        <Icon
-          className='h-6 w-6'
-          name={icon}
-        />
-
-        <div className='relative flex items-center justify-center overflow-hidden'>
-          <ArrowUpIcon className='h-3 w-3 rotate-45 transition-[transform,opacity] duration-500 ease-backOut group-hover:-translate-y-full group-hover:translate-x-full group-hover:opacity-0' />
-
-          <ArrowUpIcon className='absolute h-3 w-3 -translate-x-full translate-y-full rotate-45 opacity-0 transition-[transform,opacity] duration-500 ease-backOut group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100' />
-        </div>
-      </div>
+      <Icon
+        className='h-6 w-6'
+        name={icon}
+      />
 
       <span className='text-xs'>{capitalize(name)}</span>
+
+      <div className='absolute bottom-0 left-3.5 translate-y-1/2 rounded-full border border-transparent p-1.5 transition-transform duration-500 ease-backOut group-hover:translate-x-1/4'>
+        <ArrowUpIcon className='relative z-10 h-3 w-3 rotate-45' />
+
+        <span className='absolute -inset-px rounded-inherit border bg-main transition-colors' />
+      </div>
     </MagneticLink>
   );
 };
