769285ed1c10f07dcc59122f1274be4823b16a65
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:56:05 -0300

    Changing image animation

--- src/components/projects/grid/Image.jsx
+++ src/components/projects/grid/Image.jsx
@@ -5,12 +5,35 @@ import { useContext } from 'react';
 import { ProjectsContext } from '@/contexts';
 import { cn } from '@/utils';
 
+import ScrollAnimation from '../../scroll-animation';
 import { Image } from '../../ui';
 
 const ProjectsGridImage = ({ index, className, ...props }) => {
   const { images } = useContext(ProjectsContext);
 
   const img = images[index];
+
+  const animationConfig = {
+    clipPath: {
+      useScrollConfig: {
+        offset: ['0 1', '0 .35'],
+      },
+      useScrollRes: 'scrollYProgress',
+      prop: 'clipPath',
+      scrollPoints: [0, 1],
+      propPoints: [
+        'inset(50% 0 0 0 round 50% 50% 0 0)',
+        'inset(0% 0 0 0 round 0% 0% 0 0)',
+      ],
+    },
+    scale: {
+      useScrollConfig: {
+        offset: ['0 1', '0 .35'],
+      },
+      prop: 'scale',
+      propPoints: [1.1, 1],
+    },
+  };
 
   return (
     <div
@@ -20,11 +43,15 @@ const ProjectsGridImage = ({ index, className, ...props }) => {
       )}
       {...props}
     >
-      {/* eslint-disable-next-line jsx-a11y/alt-text */}
-      <Image
-        className='h-full w-full object-cover'
-        {...img}
-      />
+      <ScrollAnimation config={animationConfig.clipPath}>
+        <ScrollAnimation.Transform config={animationConfig.scale}>
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
+          <Image
+            className='h-full w-full object-cover'
+            {...img}
+          />
+        </ScrollAnimation.Transform>
+      </ScrollAnimation>
 
       <span className='absolute inset-0 -z-10 rounded-inherit bg-muted transition-bg' />
     </div>
