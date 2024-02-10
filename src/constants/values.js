ffc29da31320f12a8ac47ba66b3b8cfa9090666c
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 13:58:33 -0300

    Creating values constants

---
+++ src/constants/values.js
@@ -0,0 +1,28 @@
+const values = [
+  {
+    icon: 'Grid',
+    title: 'Lorem ipsum 00',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+  },
+  {
+    icon: 'Grid',
+    title: 'Lorem ipsum 01',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+  },
+  {
+    icon: 'Grid',
+    title: 'Lorem ipsum 02',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+  },
+  {
+    icon: 'Grid',
+    title: 'Lorem ipsum 03',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+  },
+];
+
+export default values;
