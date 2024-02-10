36e6017ff1d79dbc67e7ee28eb00f03f20aaf939
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:58:25 -0300

    Moving images

--- src/components/projects/list/Root.jsx
+++ src/components/projects/list/Root.jsx
@@ -5,48 +5,25 @@ import { useContext, useState } from 'react';
 import { ProjectsContext } from '@/contexts';
 import { cn } from '@/utils';
 
-import { FollowPointer, Image } from '../../ui';
+import { FollowPointer } from '../../ui';
+import Images from './Images';
+
+const FOLLOW_POINTER_ID = 'fp-projects-img';
 
 const ProjectsList = ({ className, ...props }) => {
   const [activeIdx, setActiveIdx] = useState(undefined),
     { images } = useContext(ProjectsContext);
 
-  const followPointerId = 'fp-projects-img';
-
   return (
-    <ProjectsContext.Provider value={{ activeIdx, setActiveIdx }}>
-      <FollowPointer.Area id={followPointerId}>
+    <ProjectsContext.Provider value={{ activeIdx, setActiveIdx, images }}>
+      <FollowPointer.Area id={FOLLOW_POINTER_ID}>
         <nav
           className={cn('w-full', className)}
           {...props}
         />
       </FollowPointer.Area>
 
-      <FollowPointer
-        className='aspect-square w-[50vmin] max-w-sm overflow-hidden rounded-lg'
-        id={followPointerId}
-        smoothConfig={{ stiffness: 100 }}
-      >
-        <div>
-          <ul
-            className='h-full w-full transform transition-transform duration-500'
-            style={{ '--tw-translate-y': -100 * activeIdx + '%' }}
-          >
-            {images.map((img) => (
-              <li
-                className='relative h-full w-full'
-                key={img.src}
-              >
-                {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                <Image
-                  className='h-full w-full object-cover object-center'
-                  {...img}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </FollowPointer>
+      <Images id={FOLLOW_POINTER_ID} />
     </ProjectsContext.Provider>
   );
 };
