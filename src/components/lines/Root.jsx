dd217183a3c24d43f3019b7d5e90cd97eb6ad2a2
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:54:46 -0300

    Opacity from 40 to 20

--- src/components/lines/Root.jsx
+++ src/components/lines/Root.jsx
@@ -6,7 +6,7 @@ const Lines = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        'pointer-events-none absolute inset-y-0 left-1/2 -z-40 flex w-screen -translate-x-1/2 justify-center gap-4 opacity-60 dark:opacity-40',
+        'pointer-events-none absolute inset-y-0 left-1/2 -z-40 flex w-screen -translate-x-1/2 justify-center gap-4 opacity-60 dark:opacity-20',
         className,
       )}
       {...props}
