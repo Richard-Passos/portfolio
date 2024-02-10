646413f32fc6988ac51d53afbcdb94b0c83fbb2f
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:02:13 -0300

    Removing transition-bg

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
       <Providers.GlobalState>
         <body
           className={cn(
-            'relative min-h-screen overflow-x-clip bg-main font-app text-content transition-bg',
+            'relative min-h-screen overflow-x-clip bg-main font-app text-content',
             font.variable,
           )}
         >
