1eea011d53517b9b75e1cccc4a1ab33166e4f33f
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 15:37:06 -0300

    Trying to fix projects

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -1,7 +1,7 @@
 import { baseUrl } from '@/constants';
 
 const getProjects = async (params = '', init = {}) => {
-  const res = await fetch(`${baseUrl}/api/projects?${params}`, init);
+  const res = await fetch(`${baseUrl}/api/projects${params}`, init);
 
   const data = await res.json();
 
