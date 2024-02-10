c44e8f364fc90d8bbf693d45d9390d398c9698ea
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:00:05 -0300

    Creating projects-list-images structure

---
+++ src/components/projects/list/Images.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useContext } from 'react';
+
+import { ProjectsContext } from '@/contexts';
+import { cn } from '@/utils';
+
+import { FollowPointer, Image } from '../../ui';
+
+const ProjectsListImages = ({ className, ...props }) => {
+  const { activeIdx, images } = useContext(ProjectsContext);
+
+  return (
+    <FollowPointer
+      className={cn(
+        'aspect-square w-[50vmin] max-w-sm border border-transparent [clip-path:inset(0_0_0_0_round_theme(borderRadius.3xl))]',
+        className,
+      )}
+      smoothConfig={{ stiffness: 100 }}
+      {...props}
+    >
+      <ul>
+        {images.map((img, i) => (
+          <li
+            className='absolute inset-0 transition-[clip-path] duration-500'
+            key={img.src}
+            style={{
+              clipPath:
+                i > activeIdx
+                  ? 'inset(100% 0 0 0 round 50% 50% 0 0)'
+                  : 'inset(0 round 0)',
+            }}
+          >
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <Image
+              className='h-full w-full object-cover object-center transition-transform duration-500'
+              style={{
+                transform: i > activeIdx ? `scale(${1.1})` : `scale(${1})`,
+              }}
+              {...img}
+            />
+          </li>
+        ))}
+      </ul>
+    </FollowPointer>
+  );
+};
+
+export default ProjectsListImages;
