a16784b2311dbc55e43c3a906ea2edd5d168acc5
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 15:05:10 -0300

    Changing placeholder description

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,21 +1,18 @@
 const services = [
   {
-    icon: 'CrumpledPaper',
     title: 'Front end',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deserunt explicabo ipsam voluptates placeat fuga!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio corporis sequi, dolores nostrum, praesentium omnis commodi sint enim ducimus molestiae necessitatibus consequatur et fugiat nobis magnam asperiores culpa harum?',
   },
   {
-    icon: 'CrumpledPaper',
     title: 'Back end',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deserunt explicabo ipsam voluptates placeat fuga!',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem culpa dolor vel rerum! Quaerat delectus earum tempore accusamus itaque mollitia asperiores voluptate ea. Labore, officiis quae ad dolorum ea temporibus.',
   },
   {
-    icon: 'CrumpledPaper',
     title: 'Full stack',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deserunt explicabo ipsam voluptates placeat fuga!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad delectus dolore, veniam accusamus beatae nulla sequi numquam alias rerum vel, distinctio vero dolor necessitatibus commodi cum debitis porro, maxime ratione.',
   },
 ];
 
