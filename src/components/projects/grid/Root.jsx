bd1e23f3a542c6540d1c1bb737648a0faf3e0752
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:56:56 -0300

    Refactoring

--- src/components/projects/grid/Root.jsx
+++ src/components/projects/grid/Root.jsx
@@ -3,10 +3,7 @@ import { cn } from '@/utils';
 const ProjectsGrid = ({ className, ...props }) => {
   return (
     <ul
-      className={cn(
-        'grid w-full [--gap:clamp(theme(spacing.10),20.20vw,theme(spacing.20))] max-sm:gap-y-md',
-        className,
-      )}
+      className={cn('grid w-full gap-lg sm:grid-cols-2', className)}
       {...props}
     />
   );
