47f886d50e1377ccc10d52f41a185e4a90764d39
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-10 14:56:36 -0300

    Removing item animation and fixing position

--- src/components/projects/grid/Item.jsx
+++ src/components/projects/grid/Item.jsx
@@ -1,28 +1,18 @@
 import { cn } from '@/utils';
 
-import { ScrollAnimationTransform } from '../../scroll-animation';
-
-const ProjectsGridItem = ({ className, children, ...props }) => {
-  const animationConfig = {
-    useScrollConfig: { offset: ['0 1', '0 .75'] },
-    prop: '--y',
-    propPoints: [0.15, 0],
-  };
-
+const ProjectsGridItem = ({ className, index, style, ...props }) => {
   return (
     <li
       className={cn(
-        'w-full sm:w-[calc(50%_-_var(--gap)/2)] sm:even:ml-auto sm:[&:not(:first-child)>div]:-mt-[45%]',
+        'grid grid-cols-4 gap-6 sm:row-span-2 sm:even:col-start-2 sm:even:row-start-[calc(var(--idx)+1)]',
         className,
       )}
+      style={{
+        '--idx': index,
+        ...style,
+      }}
       {...props}
-    >
-      <ScrollAnimationTransform config={animationConfig}>
-        <div className='grid grid-cols-4 gap-6 sm:translate-y-[calc(var(--y)*100%)]'>
-          {children}
-        </div>
-      </ScrollAnimationTransform>
-    </li>
+    />
   );
 };
 
