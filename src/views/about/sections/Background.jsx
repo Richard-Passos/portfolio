d796f494d411f3b70e838abd384996698f996860
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-11 08:28:29 -0300

    Testing desing

--- src/views/about/sections/Background.jsx
+++ src/views/about/sections/Background.jsx
@@ -18,21 +18,21 @@ const AboutBackgroundSection = ({ className, ...props }) => {
 
       <Separator className='col-span-full' />
 
-      <Text className='mt-[--gap] text-3xl font-medium max-sm:col-span-full sm:col-start-2 sm:col-end-13'>
+      <Text className='mt-[--gap] text-4xl/snug font-medium max-sm:col-span-full sm:col-start-2 sm:col-end-13'>
         <TextScrollAnimation text={aboutText} />
       </Text>
 
-      <section className='space-y-[--gap] max-sm:col-span-full sm:col-start-2 sm:col-end-9'>
-        <Text className='text-2xl font-medium'>
+      <section className='space-y-[--gap] max-sm:col-span-full sm:col-start-6 sm:col-end-13'>
+        <Text className='text-2xl/snug font-medium'>
           <TextScrollAnimation text='Lorem ipsum dolor sit amet consectetur adipisicin elit. Eum, ab illo molestias distinctio dicta impedit numquam, suscipit nisi placeat doloribus modi qui ex cupiditate deserunt!' />
         </Text>
 
-        <Text className='text-2xl font-medium'>
+        <Text className='text-2xl/snug font-medium'>
           <TextScrollAnimation text='Lorem ipsum dolor sit amet consectetur adipisicin elit. Adipisci dolore omnis beatae sequi! Expedita beatae dolorum assumenda amet, rem et?' />
         </Text>
       </section>
 
-      <section className='col-start-5 col-end-13 mt-2 flex flex-col gap-[--_gap] [--_gap:theme(spacing.4)] [--half-gap:calc(var(--_gap)/2)] sm:col-start-9'>
+      <section className='col-start-5 col-end-13 mt-2 flex flex-col gap-[--_gap] [--_gap:theme(spacing.4)] [--half-gap:calc(var(--_gap)/2)] sm:col-start-2 sm:col-end-6 sm:row-start-4'>
         <Text.Subtitle className='text-xs uppercase text-muted-content'>
           • Background
         </Text.Subtitle>
