3b0dffdf362ef146f9c1105319ab1a08f99d29b9
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-16 10:45:28 -0300

    Testing design

--- src/views/work/Root.jsx
+++ src/views/work/Root.jsx
@@ -1,13 +1,44 @@
-import { Text } from '@/components/ui';
+import { Bg, Lines, NextPage } from '@/components';
 import { cn } from '@/utils';
+
+import Sections from './sections';
 
 const WorkView = ({ className, ...props }) => {
   return (
     <main
-      className={cn('flex min-h-screen items-center justify-center', className)}
+      className={cn(
+        'mx-auto flex max-w-bounds flex-col items-center justify-center',
+        className,
+      )}
       {...props}
     >
-      <Text.Title>Work page</Text.Title>
+      <Sections.Hero />
+
+      <Sections.Mission
+        id='scrollTo'
+        theme='dark'
+      />
+
+      <Sections.Services theme='light' />
+
+      <Sections.Method />
+
+      <Sections.Stats theme='dark' />
+
+      <Sections.Skills theme='light' />
+
+      <Sections.Values theme='dark' />
+
+      <div className='dark relative flex w-full items-center pb-[min(28vh,theme(spacing.28))] max-2xl:min-h-screen 2xl:h-screen 2xl:max-h-bounds'>
+        <NextPage
+          href='/about'
+          text='About me'
+        />
+
+        <Bg />
+
+        <Lines />
+      </div>
     </main>
   );
 };
