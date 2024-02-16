67c04e4ed10af462ed57f242bf587e98d5db4ee4
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-16 11:13:43 -0300

    Changing from content to description

--- src/constants/values.js
+++ src/constants/values.js
@@ -2,25 +2,25 @@ const values = [
   {
     icon: 'Grid',
     title: 'Integrity',
-    content:
-      'Lasting relationships count. I establish a partnership approach and through my actions make it clear that I am interested in their success, not just mine.',
+    description:
+      'Lasting relationships count. Through my actions I make it clear that I am interested in their success, not just mine.',
   },
   {
     icon: 'Grid',
     title: 'Lorem ipsum 01',
-    content:
+    description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
   },
   {
     icon: 'Grid',
     title: 'Lorem ipsum 02',
-    content:
+    description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
   },
   {
     icon: 'Grid',
     title: 'Lorem ipsum 03',
-    content:
+    description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
   },
 ];
