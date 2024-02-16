2f485740595e25a4d83c73b87f7cfa7dd523f155
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-16 10:37:23 -0300

    Creating new contants

---
+++ src/constants/skills.js
@@ -0,0 +1,152 @@
+const skills = {
+  hard: [
+    {
+      title: 'React.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Node.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'React Native',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Next.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'JavaScript',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Jest',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'MongoDB',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Git',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'TailwindCSS',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Styled-components',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'HTML',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'CSS',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+  ],
+  soft: [
+    {
+      title: 'React.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Node.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'React Native',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Next.js',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'JavaScript',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Jest',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'MongoDB',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Git',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'TailwindCSS',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'Styled-components',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'HTML',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+    {
+      title: 'CSS',
+      icon: 'Grid',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius beatae, cumque voluptatibus repellat rerum?',
+    },
+  ],
+};
+
+export default skills;
