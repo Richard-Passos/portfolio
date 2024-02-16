258a224d508eca68d08d532486e0710295916a63
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-16 11:48:45 -0300

    Adding title variants

--- src/components/ui/text/Title.jsx
+++ src/components/ui/text/Title.jsx
@@ -1,18 +1,35 @@
 import { Slot } from '@radix-ui/react-slot';
 import { forwardRef } from 'react';
 
-import { cn } from '@/utils';
+import { cn, cnv } from '@/utils';
 
-const TextTitle = ({ asChild, className, ...props }, ref) => {
+const TextTitle = ({ asChild, variants, className, ...props }, ref) => {
   const Tag = asChild ? Slot : 'h2';
 
   return (
     <Tag
-      className={cn('text-xl font-semibold', className)}
+      className={cn(textTitleVariants(variants), className)}
       ref={ref}
       {...props}
     />
   );
 };
 
+const textTitleSizes = {
+  md: 'text-xl font-semibold',
+  lg: 'text-[12vw]/[1] font-bold uppercase tracking-tight sm:text-[min(8vw,theme(fontSize.8xl))]/[1]',
+  xl: 'text-[14vw]/[1] font-extrabold uppercase tracking-tighter sm:text-[min(10vw,theme(fontSize.9xl))]/[1]',
+};
+
+const textTitleVariants = cnv({
+  base: '',
+  variants: {
+    size: textTitleSizes,
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+});
+
 export default forwardRef(TextTitle);
+export { textTitleSizes, textTitleVariants };
