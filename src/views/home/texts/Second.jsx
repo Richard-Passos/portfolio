7808576c2ef15c8f4312fbf0fe5d554acbb5c07e
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-17 18:58:31 -0300

    Refactoring

--- src/views/home/texts/Second.jsx
+++ src/views/home/texts/Second.jsx
@@ -7,13 +7,13 @@ const HomeViewSecondText = ({ theme, className, ...props }) => {
   return (
     <div
       className={cn(
-        'relative flex items-center justify-center py-[min(28vh,theme(spacing.28))] max-2xl:min-h-screen 2xl:h-screen 2xl:max-h-bounds',
+        'py-md relative flex items-center justify-center max-2xl:min-h-screen 2xl:h-screen 2xl:max-h-bounds',
         theme,
         className,
       )}
       {...props}
     >
-      <Text className='w-9/10 max-w-screen-lg text-4xl/tight md:text-[3.5rem]/tight'>
+      <Text className='w-9/10 max-w-screen-lg text-4xl/tight font-medium md:text-[3.5rem]/tight'>
         <TextScrollAnimation
           className='mx-auto justify-center'
           text="If you're looking for a developer who's got the skills, the passion, and a problem solving mindset to make your ideas come true. Then..."
