6c7fc31b833e1298f2e14c98b6bfce0d17278d36
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-17 18:55:27 -0300

    Add new spacing

--- src/components/values/Item.jsx
+++ src/components/values/Item.jsx
@@ -4,7 +4,7 @@ const ValuesItem = ({ className, ...props }) => {
   return (
     <li
       className={cn(
-        'flex h-full flex-col items-start gap-2 rounded-lg border bg-main p-[min(var(--p),theme(spacing.8))] transition-[background-color,border-color] [--p:8vw] md:[--p:2.5vw] md:[&:nth-child(4)]:col-start-2 lg:[&:nth-child(4)]:col-start-3',
+        'p-sm flex h-full flex-col items-start gap-2 rounded-lg border bg-main transition-[background-color,border-color] md:[&:nth-child(4)]:col-start-2 lg:[&:nth-child(4)]:col-start-3',
         className,
       )}
       {...props}
