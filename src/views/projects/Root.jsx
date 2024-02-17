605eb135d8258de378fce5e972eb015a269d1e82
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-17 18:59:45 -0300

    Refactoring

--- src/views/projects/Root.jsx
+++ src/views/projects/Root.jsx
@@ -12,7 +12,7 @@ const ProjectsView = async ({ className, ...props }) => {
   return (
     <main
       className={cn(
-        'dark-layout dark relative mx-auto flex max-w-bounds flex-col items-center gap-lg py-[min(28vh,theme(spacing.28))]',
+        'dark-layout py-md dark relative mx-auto flex max-w-bounds flex-col items-center gap-lg',
         className,
       )}
       {...props}
