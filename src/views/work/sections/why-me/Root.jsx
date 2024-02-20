c5e5e8828828d3cbda0c407ca15c27acd9033508
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-20 17:10:05 -0300

    Testing design

--- src/views/work/sections/why-me/Root.jsx
+++ src/views/work/sections/why-me/Root.jsx
@@ -10,12 +10,12 @@ const WorkViewWhyMeSection = ({ className, ...props }) => {
   return (
     <Section
       className={cn(
-        'relative flex w-full flex-col items-center justify-center',
+        'flex w-full flex-col items-center justify-center overflow-hidden',
         className,
       )}
       {...props}
     >
-      <h2 className='mb-sm flex w-full flex-col'>
+      <h2 className='mb-md flex w-full flex-col'>
         <ScrollTitle title='WHY' />
         <ScrollTitle
           dir='rtl'
@@ -24,12 +24,12 @@ const WorkViewWhyMeSection = ({ className, ...props }) => {
       </h2>
 
       <section className='mb-lg flex w-9/10 max-w-screen-xl justify-between gap-sm max-sm:flex-col'>
-        <Text className='max-w-lg text-4xl/tight font-medium sm:text-5xl/tight'>
+        <Text className='max-w-lg text-4xl/tight font-medium max-sm:text-center md:text-5xl/tight'>
           <TextScrollAnimation text='We help our clients entertain, inform, and inspire the world.' />
         </Text>
 
-        <div className='flex flex-col items-start justify-between gap-md'>
-          <Text className='max-w-lg text-muted-content'>
+        <div className='flex flex-col items-start justify-between gap-md max-sm:items-center'>
+          <Text className='max-w-lg text-muted-content max-sm:text-center'>
             We represent storytellers who shape culture and drive the future.
             From artists and creators to athletes and brands, our deep expertise
             and broad capabilities enable talent and companies to confidently
