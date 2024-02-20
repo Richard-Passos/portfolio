20ccfc48fd0115008ddc4cca9f567b60da4143b6
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-20 16:32:34 -0300

    Creating stats constant

---
+++ src/constants/stats.js
@@ -0,0 +1,20 @@
+const stats = [
+  {
+    value: '10ᐩ',
+    title: `Projects \n completed`,
+  },
+  {
+    value: '1ᐩ',
+    title: `Years of \n experience`,
+  },
+  {
+    value: '100%',
+    title: `Awesome \n projects`,
+  },
+  {
+    value: '50ᐩ',
+    title: `Projects \n completed`,
+  },
+];
+
+export default stats;
