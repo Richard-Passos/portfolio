385c141201cdf5cbcbfcb56cc3e4b28114847554
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 11:48:46 -0300

    Testing values design

--- src/components/values/Item.jsx
+++ src/components/values/Item.jsx
@@ -4,7 +4,7 @@ const ValuesItem = ({ className, ...props }) => {
   return (
     <li
       className={cn(
-        'flex h-full flex-col items-start gap-2 rounded-lg border bg-main p-sm transition-[background-color,border-color] md:[&:nth-child(4)]:col-start-2 lg:[&:nth-child(4)]:col-start-3',
+        'flex h-full flex-col items-start gap-2 rounded-lg border bg-main p-sm transition-[background-color,border-color] lg:[&:nth-child(3)]:col-start-2',
         className,
       )}
       {...props}
