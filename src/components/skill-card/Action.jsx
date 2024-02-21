14967b110515332aff1247e0b1a2f214446b5be8
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 19:04:54 -0300

    Add new button design

--- src/components/skill-card/Action.jsx
+++ src/components/skill-card/Action.jsx
@@ -1,20 +1,16 @@
 import { cn } from '@/utils';
 
-const SkillCardAction = ({ className, children, ...props }) => {
+import { Button } from '../button';
+
+const SkillCardAction = ({ className, ...props }) => {
   return (
-    <div
+    <Button
       className={cn(
-        'relative isolate flex size-8 items-center justify-center overflow-hidden rounded-full bg-muted transition-colors [clip-path:circle(100%)] group-hover:text-primary-content',
+        'size-8 bg-muted px-0 text-content [--variant-a:--primary] group-hover:text-primary-content group-hover:delay-100 [&_svg]:size-1/2',
         className,
       )}
       {...props}
-    >
-      {children}
-
-      <div className='absolute inset-0 -z-10 translate-y-[101%] rounded-[50%_50%_0_0] transition-[transform,border-radius] duration-500 group-hover:translate-y-0 group-hover:rounded-[0]'>
-        <span className='absolute inset-0 rounded-inherit bg-primary transition-bg' />
-      </div>
-    </div>
+    />
   );
 };
 export default SkillCardAction;
