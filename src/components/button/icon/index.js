e5eb5aaefef432887be93d0f80345fc655ee6557
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 19:02:08 -0300

    Exporting

--- src/components/button/icon/index.js
+++
@@ -1,9 +0,0 @@
-import IconButtonIcon from './Icon';
-import IconButtonRoot from './Root';
-
-const IconButton = IconButtonRoot;
-
-IconButton.Icon = IconButtonIcon;
-
-export default IconButton;
-export { IconButtonRoot as IconButton, IconButtonIcon };
