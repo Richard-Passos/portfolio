0f1a8dd6ae57f3900ceb85c0c9d30b466eeb1f38
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 19:01:30 -0300

    Add new button design

--- src/components/back-top/Root.jsx
+++ src/components/back-top/Root.jsx
@@ -4,39 +4,34 @@ import { useLenis } from '@studio-freight/react-lenis';
 
 import { cn, isFunctionThanCall } from '@/utils';
 
-import { IconButton } from '../button';
-import { Link } from '../ui';
+import Button from '../button';
 import { ArrowUpIcon } from '../ui/icon/icons';
 
 const BackTop = ({ className, ...props }) => {
   const lenis = useLenis();
 
   return (
-    <IconButton
-      asChild
+    <Button
+      asLink
       className={cn('text-xs font-bold uppercase', className)}
-      variants={{ color: 'inverted', style: 'transparent', size: 'sm' }}
+      href='#start'
+      onClick={(ev) => {
+        lenis.scrollTo('start', {
+          duration: 2,
+          easing: (x) =>
+            x < 0.5 ? 8 * x ** 4 : 1 - Math.pow(-2 * x + 2, 4) / 2, // easeInOutQuart
+        });
+
+        isFunctionThanCall(props.onClick, ev);
+      }}
+      variants={{ color: 'main', size: 'sm' }}
       {...props}
     >
-      <Link
-        className='no-underline'
-        href='#start'
-        onClick={(ev) => {
-          lenis.scrollTo('start', {
-            duration: 2,
-            easing: (x) =>
-              x < 0.5 ? 8 * x ** 4 : 1 - Math.pow(-2 * x + 2, 4) / 2, // easeInOutQuart
-          });
-
-          isFunctionThanCall(props.onClick, ev);
-        }}
-      >
-        <IconButton.Icon animation='slideUp'>
-          <ArrowUpIcon />
-        </IconButton.Icon>{' '}
-        Back top
-      </Link>
-    </IconButton>
+      <Button.Icon animation='slideUp'>
+        <ArrowUpIcon />
+      </Button.Icon>
+      Back top
+    </Button>
   );
 };
 
