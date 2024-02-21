dc2e6230be7644891f8758167ca11ada4d9cd4b4
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 19:05:21 -0300

    Add new button design

--- src/components/ui/carousel/Action.jsx
+++ src/components/ui/carousel/Action.jsx
@@ -1,26 +1,23 @@
 import { cn } from '@/utils';
 
-const CarouselAction = ({ type = 'next', className, children, ...props }) => {
+import Button from '../../button';
+
+const CarouselAction = ({ type = 'next', className, variants, ...props }) => {
   const types = {
     next: 'splide__arrow--next',
     prev: 'splide__arrow--prev',
   };
 
   return (
-    <button
+    <Button
       className={cn(
-        'splide__arrow group relative isolate flex aspect-square w-10 items-center justify-center overflow-hidden rounded-sm bg-muted transition-colors hover:text-primary-content disabled:pointer-events-none disabled:opacity-50',
+        'splide__arrow aspect-square rounded-sm bg-muted px-0 text-content [--variant-a:--primary] hover:text-primary-content',
         types[type],
         className,
       )}
+      variants={{ size: 'sm', ...variants }}
       {...props}
-    >
-      {children}
-
-      <div className='absolute inset-0 -z-10 translate-y-[101%] rounded-[50%_50%_0_0] transition-[transform,border-radius] duration-500 group-hover:translate-y-0 group-hover:rounded-[0]'>
-        <span className='absolute inset-0 rounded-inherit bg-primary transition-bg' />
-      </div>
-    </button>
+    />
   );
 };
 
