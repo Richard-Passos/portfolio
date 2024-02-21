db9110023446bca2739a067d1d321cda480707d2
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-21 19:01:03 -0300

    Moving IconButton and adding new design

---
+++ src/components/button/Icon.jsx
@@ -0,0 +1,47 @@
+import { Slot } from '@radix-ui/react-slot';
+
+import { cn, cnv } from '@/utils';
+
+const ButtonIcon = ({ className, animation, children, ...props }) => {
+  return (
+    <div
+      aria-hidden
+      className={cn(
+        'relative flex aspect-square h-[56%] items-center justify-center transition-[clip-path] duration-500 ease-backOut [clip-path:inset(37.5%_round_theme(borderRadius.full))] group-hover:[clip-path:inset(0_round_theme(borderRadius.full))]',
+        className,
+      )}
+      {...props}
+    >
+      <span className='absolute inset-0 rounded-full bg-variant-content transition-bg' />
+
+      <Slot
+        className={cn(
+          'relative z-10 size-1/2 text-variant',
+          buttonIconAnimationVariants({ animation }),
+        )}
+      >
+        {children}
+      </Slot>
+    </div>
+  );
+};
+
+const buttonIconAnimations = {
+  opacity: 'opacity-0 transition-opacity group-hover:opacity-100',
+  slideUp: 'translate-y-full transition-transform group-hover:translate-y-0',
+  slideUpRight:
+    '-translate-x-full translate-y-full transition-transform group-hover:translate-x-0 group-hover:translate-y-0',
+};
+
+const buttonIconAnimationVariants = cnv({
+  base: 'duration-500 ease-backOut ',
+  variants: {
+    animation: buttonIconAnimations,
+  },
+  defaultVariants: {
+    animation: 'opacity',
+  },
+});
+
+export default ButtonIcon;
+export { buttonIconAnimations, buttonIconAnimationVariants };
---
+++ src/components/button/Root.jsx
@@ -0,0 +1,50 @@
+import { forwardRef } from 'react';
+
+import { cn } from '@/utils';
+
+import { Link } from '../ui';
+import ButtonUi from '../ui/button';
+
+const Button = (
+  { children, asLink, variants = {}, className, ...props },
+  ref,
+) => {
+  const content = (
+    <>
+      <span
+        className={cn(
+          'relative z-10 flex size-full items-center justify-center rounded-inherit',
+          buttonSizes[variants.size || 'md'],
+        )}
+      >
+        {children}
+      </span>
+
+      <span className='absolute inset-0 translate-y-full rounded-[50%_50%_0_0] bg-variant-active transition-[transform,border-radius] duration-500 group-hover:translate-y-0 group-hover:rounded-none' />
+    </>
+  );
+
+  return (
+    <ButtonUi
+      asChild={asLink}
+      className={cn(
+        'group relative overflow-hidden hover:delay-100',
+        className,
+      )}
+      ref={ref}
+      variants={variants}
+      {...props}
+    >
+      {asLink ? <Link className='no-underline'>{content}</Link> : content}
+    </ButtonUi>
+  );
+};
+
+const buttonSizes = {
+  sm: 'gap-x-2',
+  md: 'gap-x-3',
+  lg: 'gap-x-4',
+};
+
+export default forwardRef(Button);
+export { buttonSizes };
