82570a6e230e34f127a7e3da11168843c737a6af
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-22 17:36:21 -0300

    Testing design

--- src/views/work/sections/Projects.jsx
+++ src/views/work/sections/Projects.jsx
@@ -30,12 +30,9 @@ const WorkViewProjectsSection = ({ theme, className, ...props }) => {
         />
       </h2>
 
-      <div className='mb-lg w-9/10 max-w-screen-xl'>
-        <Text className='max-w-xl text-xl sm:ml-auto md:text-2xl'>
-          <TextScrollAnimation
-            className='max-sm:text-center'
-            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quo id vel recusandae a porro pariatur, aliquid, soluta placeat reprehenderit error velit dolor dicta laborum!'
-          />
+      <div className='mb-lg w-9/10 max-w-screen-lg'>
+        <Text className='text-lg font-medium max-sm:text-center sm:ml-auto sm:max-w-xl md:text-xl'>
+          <TextScrollAnimation text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quo id vel recusandae a porro pariatur, aliquid, soluta placeat reprehenderit error velit dolor dicta laborum!' />
         </Text>
       </div>
 
