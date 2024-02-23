9b994f64dabd229cd20d28a682a0a47235735662
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-23 11:02:25 -0300

    Add icons rotate animation

--- src/views/about/sections/Values.jsx
+++ src/views/about/sections/Values.jsx
@@ -1,45 +1,73 @@
 import { Bg, Lines, Values } from '@/components';
+import { ScrollAnimationTransform } from '@/components/scroll-animation';
 import { Badge, Icon, Text } from '@/components/ui';
+import { GlobeIcon, SmileIcon } from '@/components/ui/icon/icons';
 import { values } from '@/constants';
 import { cn } from '@/utils';
 
 const AboutViewValuesSection = ({ theme, className, ...props }) => {
+  const animationConfig = {
+    useScrollRes: 'scrollY',
+    prop: '--rotate',
+    scrollPoints: [0, 400],
+    propPoints: ['0deg', '360deg'],
+    useTransformConfig: { clamp: false },
+  };
+
   return (
-    <section
-      className={cn('relative w-9/10 max-w-screen-xl pb-lg', theme, className)}
-    >
-      <Text.Title
-        className='mb-md text-7xl font-extrabold uppercase tracking-tight sm:text-8xl'
-        id='carousel-skills-heading-0'
+    <ScrollAnimationTransform config={animationConfig}>
+      <section
+        className={cn(
+          'relative flex w-full flex-col items-center gap-md pb-lg',
+          theme,
+          className,
+        )}
         {...props}
       >
-        My <br />{' '}
-        <span className='relative inline'>
-          values{' '}
-          <Badge className='absolute bottom-0 right-0 -translate-x-4 -rotate-12 border-variant-content px-[1.5em] py-[.75em] text-[.17em] font-semibold normal-case tracking-normal'>
-            Always improving
-          </Badge>
-        </span>
-      </Text.Title>
+        <Text.Title
+          className='w-9/10 max-w-screen-xl text-7xl font-extrabold uppercase tracking-tight sm:text-8xl'
+          id='carousel-skills-heading-0'
+        >
+          My <br />{' '}
+          <span className='relative inline'>
+            values{' '}
+            <Badge className='absolute bottom-0 right-0 -translate-x-4 -rotate-12 border-variant-content px-[1.5em] py-[.75em] text-[.17em] font-semibold normal-case tracking-normal'>
+              Always improving
+            </Badge>
+          </span>
+        </Text.Title>
 
-      <Values>
-        {values.map(({ icon, title, description }) => (
-          <Values.Item key={title}>
-            <Values.Icon>
-              <Icon name={icon} />
-            </Values.Icon>
+        <div className='relative w-full'>
+          <Values className='mx-auto w-9/10 max-w-screen-xl'>
+            {values.map(({ icon, title, description }) => (
+              <Values.Item key={title}>
+                <Values.Icon>
+                  <Icon name={icon} />
+                </Values.Icon>
 
-            <Values.Title>{title}</Values.Title>
+                <Values.Title>{title}</Values.Title>
 
-            <Values.Description>{description}</Values.Description>
-          </Values.Item>
-        ))}
-      </Values>
+                <Values.Description>{description}</Values.Description>
+              </Values.Item>
+            ))}
+          </Values>
 
-      <Bg />
+          <GlobeIcon
+            aria-hidden
+            className='absolute right-0 top-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] -rotate-[--rotate] opacity-10 dark:opacity-5 max-lg:hidden'
+          />
 
-      <Lines />
-    </section>
+          <SmileIcon
+            aria-hidden
+            className='absolute bottom-0 left-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] rotate-[--rotate] opacity-10 dark:opacity-5 max-lg:hidden'
+          />
+        </div>
+
+        <Bg />
+
+        <Lines />
+      </section>
+    </ScrollAnimationTransform>
   );
 };
 
--- src/views/work/sections/Values.jsx
+++ src/views/work/sections/Values.jsx
@@ -1,41 +1,65 @@
 import { Section, Values } from '@/components';
+import { ScrollAnimationTransform } from '@/components/scroll-animation';
 import { Badge, Icon, Text } from '@/components/ui';
+import { GlobeIcon, SmileIcon } from '@/components/ui/icon/icons';
 import { values } from '@/constants';
 import { cn } from '@/utils';
 
 const WorkViewValuesSection = ({ className, ...props }) => {
+  const animationConfig = {
+    useScrollRes: 'scrollY',
+    prop: '--rotate',
+    scrollPoints: [0, 400],
+    propPoints: ['0deg', '360deg'],
+    useTransformConfig: { clamp: false },
+  };
+
   return (
-    <Section
-      className={cn('w-9/10 max-w-screen-xl space-y-md', className)}
-      {...props}
-    >
-      <Text.Title
-        className='text-7xl font-extrabold uppercase tracking-tight sm:text-8xl'
-        id='carousel-skills-heading-0'
+    <ScrollAnimationTransform config={animationConfig}>
+      <Section
+        className={cn('flex w-full flex-col items-center gap-md', className)}
+        {...props}
       >
-        My <br />{' '}
-        <span className='relative inline'>
-          values{' '}
-          <Badge className='absolute bottom-0 right-0 -translate-x-4 -rotate-12 border-variant-content px-[1.5em] py-[.75em] text-[.17em] font-semibold normal-case tracking-normal'>
-            Always improving
-          </Badge>
-        </span>
-      </Text.Title>
+        <Text.Title
+          className='w-9/10 max-w-screen-xl text-7xl font-extrabold uppercase tracking-tight sm:text-8xl'
+          id='carousel-skills-heading-0'
+        >
+          My <br />{' '}
+          <span className='relative inline'>
+            values{' '}
+            <Badge className='absolute bottom-0 right-0 -translate-x-4 -rotate-12 border-variant-content px-[1.5em] py-[.75em] text-[.17em] font-semibold normal-case tracking-normal'>
+              Always improving
+            </Badge>
+          </span>
+        </Text.Title>
 
-      <Values>
-        {values.map(({ icon, title, description }) => (
-          <Values.Item key={title}>
-            <Values.Icon>
-              <Icon name={icon} />
-            </Values.Icon>
+        <div className='relative w-full'>
+          <Values className='mx-auto w-9/10 max-w-screen-xl'>
+            {values.map(({ icon, title, description }) => (
+              <Values.Item key={title}>
+                <Values.Icon>
+                  <Icon name={icon} />
+                </Values.Icon>
 
-            <Values.Title>{title}</Values.Title>
+                <Values.Title>{title}</Values.Title>
 
-            <Values.Description>{description}</Values.Description>
-          </Values.Item>
-        ))}
-      </Values>
-    </Section>
+                <Values.Description>{description}</Values.Description>
+              </Values.Item>
+            ))}
+          </Values>
+
+          <SmileIcon
+            aria-hidden
+            className='absolute right-0 top-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] -rotate-[--rotate] opacity-10 dark:opacity-5 max-lg:hidden'
+          />
+
+          <GlobeIcon
+            aria-hidden
+            className='absolute bottom-0 left-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] rotate-[--rotate] opacity-10 dark:opacity-5 max-lg:hidden'
+          />
+        </div>
+      </Section>
+    </ScrollAnimationTransform>
   );
 };
 
