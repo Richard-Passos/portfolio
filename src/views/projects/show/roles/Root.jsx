8d9b464f651ca418431cd14db59b46fec1e12635
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-26 21:01:04 -0300

    Testing design, refactoring

--- src/views/projects/show/roles/Root.jsx
+++ src/views/projects/show/roles/Root.jsx
@@ -4,7 +4,7 @@ const ProjectsViewShowRoles = ({ className, ...props }) => {
   return (
     <section
       className={cn(
-        'isolate grid h-12 w-full max-w-sm grid-cols-3 gap-1 rounded-full border bg-main p-1 text-muted-content',
+        'isolate grid h-11 w-full max-w-sm grid-cols-3 gap-1 rounded-full border bg-main p-1 text-muted-content',
         className,
       )}
       {...props}
