4797775e106b30afd1c42c1bbae624dd77aaa0fe
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-26 21:02:29 -0300

    baseVelocity from 2 to 1

--- src/components/horizontal-scroll/Root.jsx
+++ src/components/horizontal-scroll/Root.jsx
@@ -15,7 +15,7 @@ import { useGetNumberOfSiblings } from '@/hooks';
 import { cn, setRefs } from '@/utils';
 
 const HorizontalScroll = (
-  { className, children, baseVelocity = 2, ...props },
+  { className, children, baseVelocity = 1, ...props },
   ref,
 ) => {
   const baseX = useMotionValue(0);
