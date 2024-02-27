e2d89e5b8ab3567b9406220bdf7fbb97192bae23
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-27 15:53:54 -0300

    Remove NavFallback, add fallback to languages

--- src/components/header/Root.jsx
+++ src/components/header/Root.jsx
@@ -2,13 +2,13 @@ import { Suspense } from 'react';
 
 import { cn } from '@/utils';
 
+import DotsLoader from '../dots-loader';
 import Language from '../language';
 import { MagneticLink } from '../link';
 import Menu from '../menu';
 import { Logo, Separator } from '../ui';
 import { SheetTrigger } from '../ui/sheet';
 import Nav from './Nav';
-import NavFallback from './NavFallback';
 
 const Header = ({ className, ...props }) => {
   return (
@@ -22,16 +22,16 @@ const Header = ({ className, ...props }) => {
       <Logo className='transition-none' />
 
       <div className='flex h-10 items-center max-sm:hidden'>
-        <Suspense fallback={<NavFallback />}>
-          <Nav />
-        </Suspense>
+        <Nav />
 
         <Separator
           className='mr-4'
           orientation='vertical'
         />
 
-        <Language />
+        <Suspense fallback={<DotsLoader />}>
+          <Language />
+        </Suspense>
       </div>
 
       <Menu>
