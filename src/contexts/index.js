8a66f2ebff8b58125bc0d1faef7ffcfa5f59a937
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-28 17:54:42 -0300

    Create StatsChanger context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -3,3 +3,7 @@ export { default as FormFieldContext } from './FormField';
 export { default as ProgressContext } from './Progress';
 export { default as ProjectsContext } from './Projects';
 export { default as ProjectsShowContext } from './ProjectsShow';
+export {
+  default as StatsChangerContext,
+  StatsChangerProvider,
+} from './StatsChanger';
