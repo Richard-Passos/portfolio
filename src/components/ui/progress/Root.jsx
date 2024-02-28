f54b2317b76f8ea0f37be5b4aafa5339147fbfd1
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-28 17:55:46 -0300

    Change sizes

--- src/components/ui/progress/Root.jsx
+++ src/components/ui/progress/Root.jsx
@@ -31,10 +31,10 @@ const Progress = (
 };
 
 const progressSizes = {
-  xs: 'h-1',
-  sm: 'h-2',
-  md: 'h-4',
-  lg: 'h-6',
+  xs: 'h-0.5',
+  sm: 'h-1',
+  md: 'h-2',
+  lg: 'h-4',
 };
 
 /* 'relative' and 'z-0' are to fix overflow clipping in Safari. See more here: https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0 */
