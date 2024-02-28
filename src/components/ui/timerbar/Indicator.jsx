d0731a97fc653a15bb77844338695e3a8f04526e
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-28 17:56:47 -0300

    Create variant to transition

--- src/components/ui/timerbar/Indicator.jsx
+++ src/components/ui/timerbar/Indicator.jsx
@@ -5,15 +5,21 @@ import { cn } from '@/utils';
 import { ProgressIndicator } from '../progress';
 import { TIMERBAR_UPDATE_DELAY } from './Root';
 
+const TIMERBAR_INDICATOR_TRANSITION_DURATION = TIMERBAR_UPDATE_DELAY;
+
 const TimerbarIndicator = ({ className, style, ...props }, ref) => {
   return (
     <ProgressIndicator
       className={cn('ease-linear', className)}
       ref={ref}
-      style={{ transitionDuration: TIMERBAR_UPDATE_DELAY, ...style }}
+      style={{
+        transitionDuration: TIMERBAR_INDICATOR_TRANSITION_DURATION,
+        ...style,
+      }}
       {...props}
     />
   );
 };
 
 export default forwardRef(TimerbarIndicator);
+export { TIMERBAR_INDICATOR_TRANSITION_DURATION };
