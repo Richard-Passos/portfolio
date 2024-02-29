d495a90107e77ac42b8a62fe4eaa5678457d5aa4
Richard Passos <richard.ac.passos12@gmail.com>
2024-02-29 10:30:46 -0300

    Changing images animation

--- src/views/about/sections/hero/Images.jsx
+++ src/views/about/sections/hero/Images.jsx
@@ -5,43 +5,50 @@ const SCROLL_OFFSET = ['0 1', '0 0'];
 
 const AboutViewHeroImagesSeciton = ({ className, ...props }) => {
   const animationConfig = {
-    y1: {
+    y: {
       useScrollConfig: {
         offset: SCROLL_OFFSET,
       },
+      prop: '--y',
       propPoints: ['-5%', '0%'],
     },
-    y2: {
+    x: {
       useScrollConfig: {
         offset: SCROLL_OFFSET,
       },
-      propPoints: ['0%', '-5%'],
+      prop: '--smooth-x',
+      propPoints: [1, 0],
     },
-    y3: {
+    rotate: {
       useScrollConfig: {
         offset: SCROLL_OFFSET,
       },
-      propPoints: ['-5%', '0%'],
+      prop: '--rotate',
+      propPoints: ['15deg', '0deg'],
     },
   };
 
   return (
-    <ul
-      className={cn(
-        'grid w-9/10 max-w-screen-xl gap-sm sm:grid-cols-3 lg:gap-x-md',
-        className,
-      )}
-      {...props}
-    >
-      {[...Array(3)].map((_, i) => (
-        <ScrollAnimationTransform
-          config={animationConfig['y' + (i + 1)]}
-          key={i}
-        >
-          <li className='aspect-[1/1.4] w-full rounded-3xl bg-blue-500 odd:bg-red-500 max-sm:!translate-y-0 max-sm:last:hidden sm:odd:mt-[20%]' />
+    <ScrollAnimationTransform config={animationConfig.y}>
+      <ScrollAnimationTransform config={animationConfig.x}>
+        <ScrollAnimationTransform config={animationConfig.rotate}>
+          <ul
+            className={cn(
+              'grid w-9/10 max-w-screen-xl gap-[--gap] [--gap:theme(spacing.sm)] sm:grid-cols-3 sm:[--x:calc(var(--smooth-x)*(var(--gap)+35%))] lg:[--gap:theme(spacing.md)]',
+              className,
+            )}
+            {...props}
+          >
+            {[...Array(3)].map((_, i) => (
+              <li
+                className='aspect-[1/1.4] w-full rounded-3xl bg-blue-500 odd:bg-red-500 even:z-10 max-sm:last:hidden sm:first:translate-x-[--x] sm:first:-rotate-[--rotate] sm:last:-translate-x-[--x] sm:last:rotate-[--rotate] sm:odd:mt-[25%] sm:odd:translate-y-[--y]'
+                key={i}
+              />
+            ))}
+          </ul>
         </ScrollAnimationTransform>
-      ))}
-    </ul>
+      </ScrollAnimationTransform>
+    </ScrollAnimationTransform>
   );
 };
 
