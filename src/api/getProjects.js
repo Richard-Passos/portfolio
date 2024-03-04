e36cc24add50f204b809fb7075b74cf2f89d6eef
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-04 16:01:59 -0300

    Try 2 fix error

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -3,10 +3,10 @@ import { baseUrl } from '@/constants';
 const getProjects = async (params, init) => {
   const res = await fetch(
     `${baseUrl}/api/projects${params ? '?' + params : ''}`,
-    { cache: 'no-cache' },
+    { cache: 'no-store' },
   );
 
-  const data = await res.json();
+  const { data } = await res.json();
   console.log('-  data   -', data);
 
   return data;
