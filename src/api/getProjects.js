43e581eaad0887c8f324fee7b7f0c1b203f2927d
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-04 15:55:30 -0300

    Remove cache

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -3,7 +3,7 @@ import { baseUrl } from '@/constants';
 const getProjects = async (params, init) => {
   const res = await fetch(
     `${baseUrl}/api/projects${params ? '?' + params : ''}`,
-    init,
+    { cache: 'no-cache' },
   );
 
   const { data } = await res.json();
