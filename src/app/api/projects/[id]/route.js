ba06510a07bf55518a0a54abbaffdb93050b2825
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-23 21:05:34 -0300

    Get adjacent ids and return on meta

--- src/app/api/projects/[id]/route.js
+++ src/app/api/projects/[id]/route.js
@@ -3,11 +3,15 @@ import { projects } from '@/constants';
 const GET = async (_, { params: { id } }) => {
   id = id?.toLowerCase()
 
-  const data = projects.find(({slug}) => slug === id?.replace(/[_ ]/g, '-'))
+  const dataIdx = projects.findIndex((({slug}) => slug === id?.replace(/[_ ]/g, '-')))
+
+  const data = projects[dataIdx],
+  adjacentIds = {prev: projects.at(dataIdx > 0 ? dataIdx - 1 : projects.length - 1)?.slug, next: projects.at(dataIdx < projects.length - 1 ? dataIdx + 1 : 0)?.slug}
 
   return Response.json({
     status: 200,
-    data
+    data,
+    meta: { adjacentIds }
   });
 };
 
