cc3e4f47159508663b5d488613e41a5949ddfb65
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-25 19:57:55 -0300

    Add cn

--- src/components/language/Root.jsx
+++ src/components/language/Root.jsx
@@ -11,7 +11,7 @@ import { ChevronDownIcon, ChevronUpIcon } from '../ui/icon/icons';
 const DEFAULT_LANG = languages[0]?.toLowerCase(),
   LANG_REGEX = /\/[a-z]{2}(?![^/])/;
 
-const Language = (props) => {
+const Language = ({className, ...props}) => {
   const router = useRouter(),
     pathname = usePathname(),
     searchParams = useSearchParams();
@@ -41,7 +41,7 @@ const Language = (props) => {
       onValueChange={onChange}
       {...props}
     >
-      <Select.Trigger className='h-8 w-12 justify-center gap-1 px-0 lowercase transition-none'>
+      <Select.Trigger className={cn('h-8 w-12 justify-center gap-1 px-0 lowercase transition-none', className)}>
         <Select.Value />
 
         <Select.Icon
