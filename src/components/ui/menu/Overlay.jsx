2c4c6a1d10af1fe8a7e94589fcf2624cfe9668c4
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-25 20:14:10 -0300

    Test design

--- src/components/ui/menu/Overlay.jsx
+++ src/components/ui/menu/Overlay.jsx
@@ -11,14 +11,20 @@ const MenuOverlay = ({ className, ...props }, ref) => {
   return (
     <div
       ref={ref}
+      className={cn('group absolute inset-0', className)}
       data-state={isOpen ? 'open' : 'closed'}
+    >
+      <div
       className={cn(
-        'pointer-events-auto absolute inset-0 -translate-y-full overflow-hidden rounded-[0_0_50%_50%] transition-[transform,border-radius] duration-500 data-closed:delay-300 data-open:translate-y-0 data-open:rounded-none',
+        'pointer-events-auto z-10 absolute inset-0 -translate-y-full overflow-hidden rounded-[0_0_50%_50%] transition-[transform,border-radius] duration-500 group-data-closed:delay-300 group-data-open:translate-y-0 group-data-open:rounded-none',
         className,
       )}
       {...props}
     >
       <span className='size-full bg-primary transition-bg' />
+    </div>
+
+ <span className='bg-main/25 backdrop-blur-sm transition-opacity group-data-closed:delay-500 duration-300 pointer-events-auto group-data-closed:pointer-events-none group-data-closed:opacity-0 absolute inset-0'/>
     </div>
   );
 };
