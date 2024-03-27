938a22bfe9a10b4e4f4810bebc6f6dedb61cb32e
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-27 16:12:34 -0300

    Add new hooks

--- src/contexts/Menu.jsx
+++ src/contexts/Menu.jsx
@@ -1,21 +1,41 @@
 'use client';
 
+import { useEventListener, useTimeout } from '@/hooks';
 import { createContext, useState } from 'react';
 
+const KEYS = ['Escape']
+
 const MenuContext = createContext({
-  isOpen: 0,
+  isOpen: false,
   setIsOpen: () => {},
+  isDeleted: false,
+  setIsDeleted: () => {},
 });
 
-const MenuProvider = ({ value, ...props }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const MenuProvider = ({ deleteDelay = 700, value, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false),
+  [isDeleted, setIsDeleted] = useState(true), 
+  {reset, clear} = useTimeout(() => setIsDeleted(true), deleteDelay)
 
-  return (
-    <MenuContext.Provider
-      value={{ isOpen, setIsOpen, ...value }}
+  const handleSetIsOpen = () => {
+    if(!isOpen) {
+      clear()
+
+      setIsDeleted(false)
+      setTimeout(() => setIsOpen(true), 1)
+    }
+    else {
+      reset()
+      setIsOpen(false)
+    }
+  }        
+
+        useEventListener('keyup', (ev) => KEYS.includes(ev.key) && handleSetIsOpen)
+
+  return <MenuContext.Provider
+      value={{ isOpen, setIsOpen: handleSetIsOpen, isDeleted, setIsDeleted, ...value }}
       {...props}
     />
-  );
 };
 
 export default MenuContext;
