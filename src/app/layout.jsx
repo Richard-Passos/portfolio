83eb4f67863c54040029a50150e9eb6610cf991a
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-27 19:42:26 -0300

    Remove scroll-smooth

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,10 +10,7 @@ const font = Inter({ subsets: ['latin'], variable: '--font-app' });
 
 const Layout = ({ children }) => {
   return (
-    <html
-      className='scroll-smooth'
-      lang='en'
-    >
+    <html    lang='en'>
       <Providers.GlobalState>
         <SmoothScroll>
           <body
