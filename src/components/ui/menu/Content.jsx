f31ba2939a1358167dbfd933d292d8434441530b
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-02 18:25:15 -0300

    FIX add "s" to transitions

--- src/components/ui/menu/Content.jsx
+++ src/components/ui/menu/Content.jsx
@@ -12,7 +12,7 @@ const MenuContent = ({ className, ...props }, ref) => {
     <section
       aria-hidden={!isOpen}
       className={cn(
-        'primary group/menu pointer-events-auto relative z-10 flex size-full items-center justify-center bg-main duration-500 [clip-path:inset(0_round_0)] [transition:transform_.7,clip-path_.5] data-closed:-translate-y-[20%] data-closed:[clip-path:inset(0_0_100%_0_round_0_0_50%_50%)] data-closed:[transition:transform_.5,clip-path_.3]',
+        'primary group/menu pointer-events-auto relative z-10 flex size-full items-center justify-center bg-main duration-500 [clip-path:inset(0_round_0)] [transition:transform_.7s,clip-path_.5s] data-closed:-translate-y-[20%] data-closed:[clip-path:inset(0_0_100%_0_round_0_0_50%_50%)] data-closed:[transition:transform_.5,clip-path_.3]',
         className,
       )}
       data-state={state}
