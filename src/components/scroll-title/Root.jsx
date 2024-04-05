82635a62f81adaf987d5ae53c1d06308d76a0b6c
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-05 13:52:06 -0300

    FIX leading from 1 to none

--- src/components/scroll-title/Root.jsx
+++ src/components/scroll-title/Root.jsx
@@ -29,7 +29,7 @@ const ScrollTitle = ({ dir = 'ltr', className, variants, title, ...props }) => {
     <TextTitle
       asChild
       className={cn(
-        'w-full overflow-hidden text-[16vw]/1',
+        'w-full overflow-hidden text-[16vw]/none',
         className,
       )}
       ref={containerRef}
