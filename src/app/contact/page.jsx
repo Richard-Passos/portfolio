d76e238ecb4e92e511e4472490de37bf4b92042d
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-06 09:49:33 -0300

    CHORE add data and generateMetadata

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,24 @@
+import { pagesApi } from '@/api';
+import { capitalize } from '@/utils';
 import { ContactView } from '@/views';
 
-const ContactPage = () => {
-  return <ContactView />;
+const ContactPage = async () => {
+  const {data} = await pagesApi.getOne('contact');
+
+  return <ContactView data={data} />;
 };
 
-const metadata = {
-  title: 'Contact',
-  description: 'Richard passos contact details and contact form.',
-};
+const generateMetadata = async () => {
+   const { metadata = {} } = (await pagesApi.getOne('contact')).data || {};
 
+  return {
+    title: capitalize(metadata.title),
+    description: metadata.description,
+    openGraph: {
+      title: capitalize(metadata.title),
+      description: metadata.description,
+    },
+  };
+};
 export default ContactPage;
-export { metadata };
+export { generateMetadata };
