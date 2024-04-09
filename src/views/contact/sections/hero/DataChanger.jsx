de6aa9ed9ad7f16b5e6cb42249f656e50da73c58
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-08 22:35:00 -0300

    CHORE from stats-changer to data-changer,

---
+++ src/views/contact/sections/hero/DataChanger.jsx
@@ -0,0 +1,43 @@
+import { DataChanger } from '@/components';
+import { MagneticButton } from '@/components/button';
+import { Text } from '@/components/ui';
+import { RotateIcon } from '@/components/ui/icon/icons';
+import { cn } from '@/utils';
+
+const ContactViewHeroSectionDataChanger = ({ className, data=[], ...props }) => {
+  return (
+    <DataChanger
+      className={cn('flex items-center size-full gap-xs relative overflow-hidden', className)}
+      lastIdx={data.length - 1}
+      {...props}
+    >
+      <DataChanger.Action asChild>
+        <MagneticButton
+          className='h-12 shrink-0 [&_svg]:size-[40%]'
+          variants={{ color: 'muted', size: 'sm' }}
+        >
+          <RotateIcon />
+        </MagneticButton>
+      </DataChanger.Action>
+
+        {data.map((data, i) => (
+          <DataChanger.Item
+            idx={i}
+            key={data.title}
+          >
+            <Text.Title className='text-sm uppercase transition-[clip-path] [clip-path:inset(100%_0_0_0)] group-data-active:duration-500 group-data-active:[clip-path:inset(0)]'>
+              {data.title}
+            </Text.Title>
+
+            <Text className='text-sm first-letter:uppercase text-muted-content transition-[clip-path] [clip-path:inset(100%_0_0_0)] group-data-active:duration-500 group-data-active:[clip-path:inset(0)]'>
+              {data.description}
+            </Text>
+          </DataChanger.Item>
+        ))}
+
+        <DataChanger.Timerbar className='absolute inset-x-0 w-auto bottom-0' />
+    </DataChanger>
+  );
+};
+
+export default ContactViewHeroSectionDataChanger;
