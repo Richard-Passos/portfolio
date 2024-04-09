69e7c86a133804bb9c2d9a7cf65ed0e848b07377
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-08 22:46:52 -0300

    FIX remove space when it do not have content

--- src/components/ui/form/Message.jsx
+++ src/components/ui/form/Message.jsx
@@ -10,15 +10,17 @@ import { Text } from '../text';
 const FormMessage = ({ className, children, ...props }, ref) => {
   const { messageId, error } = useFormField();
 
-  return (
+  const content = error?.message ?? children
+
+  return content && (
     <Text
       className={cn('mt-2 text-xs font-medium text-danger', className)}
       id={messageId}
       ref={ref}
       {...props}
     >
-      {error?.message ?? children}
-    </Text>
+      {content}
+    </Text> 
   );
 };
 
