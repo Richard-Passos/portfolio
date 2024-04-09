08953b30550827c203abba3b3572a519e435863b
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-08 22:38:37 -0300

    CHORE add stats to personal-info

--- src/constants/globals.js
+++ src/constants/globals.js
@@ -78,6 +78,24 @@ const globals = [
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad delectus dolore, veniam accusamus beatae nulla sequi numquam alias rerum vel, distinctio vero dolor necessitatibus commodi cum debitis porro, maxime ratione.',
       },
+    ],
+    stats: [
+      {
+        title: '10ᐩ',
+        description: `Projects \n completed`,
+      },
+      {
+        title: '1ᐩ',
+        description: `Years of \n experience`,
+      },
+      {
+        title: '100%',
+        description: `Great \n projects`,
+      },
+      {
+        title: '50ᐩ',
+        description: `Clebers \n completed`,
+      },
     ]
   },
 ];
