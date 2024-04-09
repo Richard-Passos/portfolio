7855e1bd6ac499871405550a4c62e1dcb3219119
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-08 22:38:07 -0300

    CHORE add services to personal-info

--- src/constants/globals.js
+++ src/constants/globals.js
@@ -62,6 +62,23 @@ const globals = [
       href: 'https://www.buymeacoffee.com/richardp.dev',
     },
     availability: 'Available to work',
+    services: [
+      {
+        title: 'Front end',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio corporis sequi, dolores nostrum, praesentium omnis commodi sint enim ducimus molestiae necessitatibus consequatur et fugiat nobis magnam asperiores culpa harum?',
+      },
+      {
+        title: 'Back end',
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem culpa dolor vel rerum! Quaerat delectus earum tempore accusamus itaque mollitia asperiores voluptate ea. Labore, officiis quae ad dolorum ea temporibus.',
+      },
+      {
+        title: 'Full stack',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad delectus dolore, veniam accusamus beatae nulla sequi numquam alias rerum vel, distinctio vero dolor necessitatibus commodi cum debitis porro, maxime ratione.',
+      },
+    ]
   },
 ];
 
