74549964079f206994ce268e58ef54124a60aa4d
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-10 17:26:59 -0300

    CHORE add data, REFA

--- src/views/home/sections/Work.jsx
+++ src/views/home/sections/Work.jsx
@@ -7,62 +7,65 @@ import {
   TextScrollAnimate,
 } from '@/components';
 import { MagneticButton } from '@/components/button';
-import { Text } from '@/components/ui';
-import { PlusIcon } from '@/components/ui/icon/icons';
+import { Icon, Text } from '@/components/ui';
 import { cn } from '@/utils';
 
-const HomeViewWorkSection = ({ className, ...props }) => {
+const HomeViewWorkSection = ({ className, data = {}, ...props }) => {
+  const { block = {} } = data
+
   return (
     <Section
       className={cn('flex flex-col items-center', className)}
       {...props}
     >
-      <h2 className='mb-md flex w-full flex-col'>
-        <ScrollTitle title='ABOUT' />
-        <ScrollTitle
-          dir='rtl'
-          title='WORK'
-        />
+      <h2 className='w-full'>
+        {data.title?.map((w, i) => (
+          <ScrollTitle
+            dir={i % 2 === 0 ? 'ltr' : 'rtl'}
+            key={i}
+            title={w}
+          />
+        ))}
       </h2>
 
-      <section className='mb-lg grid w-9/10 max-w-screen-xl gap-sm sm:grid-cols-2'>
+      <section className='mt-md grid w-9/10 max-w-screen-xl gap-sm sm:grid-cols-2'>
         <Text className='text-4xl/tight font-medium max-sm:text-center sm:max-w-lg md:text-5xl/tight'>
-          <TextScrollAnimate text='We help our clients entertain, inform, and inspire the world.' />
+          <TextScrollAnimate
+            className='first:first-letter:uppercase'
+            text={data.subtitle}
+          />
         </Text>
 
-        <Text className='text-muted-content max-sm:text-center sm:max-w-lg sm:justify-self-end'>
-          We represent storytellers who shape culture and drive the future. From
-          artists and creators to athletes and brands, our deep expertise and
-          broad capabilities enable talent and companies to confidently grow
-          their careers and businesses.
+        <Text className='text-muted-content first-letter:uppercase max-sm:text-center sm:max-w-lg sm:justify-self-end'>
+          {data.description}
         </Text>
       </section>
 
-      <div className='mb-lg flex w-9/10 max-w-screen-lg flex-col items-center gap-md'>
-        <Text.Subtitle className='mr-auto text-xs uppercase text-muted-content'>
-          Selected works
+      <div className='mt-lg flex w-9/10 max-w-screen-lg flex-col items-center'>
+        <Text.Subtitle className='text-xs uppercase text-muted-content'>
+          {block.title}
         </Text.Subtitle>
 
-        <ShowProjects />
+        <ShowProjects className='mt-md' />
 
         <MagneticButton
-          aria-label='More works'
-          href='/projects'
-          variants={{ color: 'main' }}
+        className='mt-md'
+          aria-label={block.action?.label}
+          {...(block.action || {})}
         >
-          <PlusIcon aria-hidden />
+          <Icon aria-hidden {...block.action?.icon} />
         </MagneticButton>
       </div>
 
-      <ListWorkPage />
+      <ListWorkPage data={data.list} className='mt-lg' />
     </Section>
   );
 };
 
 const ShowProjects = async ({ className, ...props }) => {
-  const selectedProjects = (await projectsApi.getSelecteds()).data || [];
+  const { data = {} } = await projectsApi.getSelecteds()
 
-  const projects = selectedProjects.reduce(
+  const projects = data.reduce(
     (obj, { thumbnail, ...data }) => ({
       data: [...obj.data, data],
       images: [...obj.images, thumbnail],
@@ -117,8 +120,8 @@ const ShowProjects = async ({ className, ...props }) => {
   );
 };
 
-const ListWorkPage = ({ className, ...props }) => {
-  const content = ['Mission', 'Soft & hard skills', 'Services'];
+const ListWorkPage = ({ className, data = {}, ...props }) => {
+  const { action = {} } = data
 
   return (
     <div
@@ -126,22 +129,22 @@ const ListWorkPage = ({ className, ...props }) => {
       {...props}
     >
       <ListHorizontalScroll>
-        {content.map((content, i) => (
+        {data.items?.map((data, i) => (
           <ListHorizontalScroll.Item
             baseVelocity={(1 + 0.35 * i) * (i % 2 === 0 ? 1 : -1)}
             className='[--gap:theme(spacing.4)]'
-            key={content}
+            key={data}
           >
-            <span>{content}</span> ·{' '}
-            <span className='opacity-30 dark:opacity-10'>{content}</span>{' '}
+            <span>{data}</span> ·{' '}
+            <span className='opacity-30 dark:opacity-10'>{data}</span>{' '}
             <span className='opacity-30 dark:opacity-10'>·</span>{' '}
-            <span className='opacity-30 dark:opacity-10'>{content}</span> ·
+            <span className='opacity-30 dark:opacity-10'>{data}</span> ·
           </ListHorizontalScroll.Item>
         ))}
       </ListHorizontalScroll>
 
-      <ListHorizontalScroll.Link href='/work'>
-        Explore
+      <ListHorizontalScroll.Link className='mt-md' {...action}>
+        {action.label}
       </ListHorizontalScroll.Link>
     </div>
   );
