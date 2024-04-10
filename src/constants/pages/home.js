4c84e36ea728c14947342b085a154193a617963d
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-10 20:52:54 -0300

    CHORE add data

--- src/constants/pages/home.js
+++ src/constants/pages/home.js
@@ -5,7 +5,7 @@ const homePage = {
       slug: 'hero',
       theme: 'light',
       data: {
-        title: `Turning heads \n<bold>&</bold> conquering \nhearts`,
+        title: `Turning heads \n<b>&</b> conquering \nhearts`,
         description: "Hey there! I'm Richard an awesome full stack developer — who cares building solid and scalable products with a great user experience.",
         year: 2024
       }
@@ -60,6 +60,65 @@ const homePage = {
           'About',
           'me'
         ],
+        grid: {
+          templates: {
+            default: '"item-0""item-1""item-2""item-3""item-4""item-5""item-6"',
+            sm: '"item-0 item-0""item-1 item-2""item-3 item-4""item-5 item-6"',
+            lg: '". item-0 item-0"". item-0 item-0""item-1 item-2 item-3""item-1 item-2 item-4""item-5 item-5 item-6""item-5 item-5 item-6"'
+          },
+          items: [
+            {
+              type: 'description',
+              data: {
+                title: 'About',
+                description: "🤟 <b>Hey —</b> I'm Richard an awesome full stack developer based in Brazil. When I'm not coding, you can catch me in the gaming world — I'm a huge fan, especially when it comes to rogue-like games."
+              }
+            },
+            {
+              type: 'bold',
+              data: {
+                icon: {src: '/images/globe.svg', title: 'Globe'},
+                description: "Based in Brazil, GMT-3"
+              }
+            },
+            {
+              type: 'infinity-scroll',
+              data: {
+                icon: {src: '/images/fingerprint.svg', title: 'Fingerprint'},
+                title: 'My values',
+                items: ['Integrity', 'Curiosity', 'Respect', 'Gratitude']
+              }
+            },
+            {
+              type: 'simple',
+              data: {
+                icon: {src: '/images/rocket.svg', title: 'Rocket'},
+                description: 'Improving a little bit every day.',
+              }
+            },
+            {
+              type: 'simple',
+              data: {
+                icon: {src: '/images/game-controller.svg', title: 'Game controller'},
+                description: 'I love to play games.',
+              }
+            },
+            {
+              type: 'local-time',
+              data: {
+                title: 'My local time'
+              }
+            },
+            {
+              type: 'link',
+              data: {
+                href: 'https://www.buymeacoffee.com/richardp.dev',
+                icon: {src: '/images/mug.svg', title: 'Mug'},
+                title: 'Buy me a coffee'
+              }
+            }
+          ]
+        },
         list: {
           items: ['Background', 'Hobbys', 'Personality'],
           action: {
