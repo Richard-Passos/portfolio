c6cde97aa4fe18b6f0c60c2ab2af75c2b646adcb
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:13:27 -0300

    CHORE remove unnecessary code

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,4 @@ export { default as globals } from './globals';
 export { default as languages } from './languages';
 export { default as pages } from './pages';
 export { default as projects } from './projects';
-export { default as services } from './services';
 export { default as skills } from './skills';
-export { default as stats } from './stats';
-export { default as values } from './values';
