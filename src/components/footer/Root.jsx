ed814f42a5f3ef2ad9db62cbd4bebcb3cae92abc
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:27:02 -0300

    CHORE pt from lg to lg * .5

--- src/components/footer/Root.jsx
+++ src/components/footer/Root.jsx
@@ -8,16 +8,16 @@ const Footer = ({ className, ...props }) => {
   return (
     <footer
       className={cn(
-        'relative flex w-full max-w-bounds flex-col items-center overflow-y-clip pt-lg',
+        'relative flex w-full max-w-bounds flex-col items-center overflow-y-clip pt-[--pt] [--pt:calc(theme(spacing.lg)*.5)]',
         className,
       )}
       {...props}
     >
-      <div className='dark absolute h-lg top-0 w-screen bg-main'>
+      <div className='dark absolute h-[--pt] top-0 w-screen bg-main'>
       <Lines className='z-0'/>
       </div>
 
-      <Overlay theme='dark' className='top-lg' />
+      <Overlay theme='dark' className='top-[--pt]' />
 
       <Content theme='light' />
     </footer>
