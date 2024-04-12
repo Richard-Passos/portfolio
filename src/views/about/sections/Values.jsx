2e8065b0d7ce1df7c5574693880452b7795260f0
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:26:48 -0300

    REFA, CHORE add dynamicity

--- src/views/about/sections/Values.jsx
+++ src/views/about/sections/Values.jsx
@@ -1,7 +1,6 @@
 import { Section, Values } from '@/components';
 import { ScrollAnimateTransform } from '@/components/scroll-animate';
 import { Badge, Icon, Text } from '@/components/ui';
-import { GlobeIcon, SmileIcon } from '@/components/ui/icon/icons';
 import { cn } from '@/utils';
 
 const ANIMATION_CONFIG = {
@@ -23,12 +22,12 @@ const ANIMATION_CONFIG = {
 
 const AboutViewValuesSection = ({ className, data = {}, ...props }) => {
   return (
-    <Section
-      className={cn('flex flex-col items-center', className)}
-      {...props}
-    >
-      <Text.Title
-        aria-label={data.tile}
+      <Section
+        className={cn('flex flex-col items-center', className)}
+        {...props}
+      >
+        <Text.Title
+        aria-label={data.title}
         className='w-9/10 max-w-screen-xl whitespace-pre-line text-[16vw]/none'
         variants={{ size: 'lg' }}
       >
@@ -52,40 +51,41 @@ const AboutViewValuesSection = ({ className, data = {}, ...props }) => {
 
       <span className='sr-only'>{data.subtitle}</span>
 
-      <div className='relative mt-md w-full'>
-        <Values className='mx-auto w-9/10 max-w-screen-xl'>
-          {data.items?.map((data) => (
-            <Values.Item key={data.title}>
-              <Values.Icon>
-                <Icon
-                  aria-hidden
-                  {...data.icon}
-                />
-              </Values.Icon>
-
-              <Values.Title>{data.title}</Values.Title>
-
-              <Values.Description>{data.description}</Values.Description>
-            </Values.Item>
-          ))}
-        </Values>
-
-        <ScrollAnimateTransform config={ANIMATION_CONFIG.rotate1}>
-          <GlobeIcon
-            aria-hidden
-            className='absolute right-0 top-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] text-muted max-lg:hidden'
-          />
-        </ScrollAnimateTransform>
-
-        <ScrollAnimateTransform config={ANIMATION_CONFIG.rotate2}>
-          <SmileIcon
-            aria-hidden
-            className='absolute bottom-0 left-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] text-muted max-lg:hidden'
-          />
-        </ScrollAnimateTransform>
-      </div>
-    </Section>
+<AboutViewValuesSectionBlock className='mt-md' data={data.block}/>
+      </Section>
   );
 };
+
+const AboutViewValuesSectionBlock = ({ className, data = {}, ...props }) => {
+  return <div className={cn('relative w-full', className)} {...props}>
+  <Values className='mx-auto w-9/10 max-w-screen-xl'>
+    {data.items?.map((data) => (
+      <Values.Item key={data.title}>
+        <Values.Icon>
+          <Icon
+            aria-hidden
+            {...data.icon}
+          />
+        </Values.Icon>
+
+        <Values.Title>{data.title}</Values.Title>
+
+        <Values.Description>{data.description}</Values.Description>
+      </Values.Item>
+    ))}
+  </Values>
+
+  
+
+{data.icons?.map((data, i) => <ScrollAnimateTransform config={ANIMATION_CONFIG[`rotate${i % 2 === 0 ? 1 : 2}`]}>
+    <div className='absolute first-of-type:right-0 first-of-type:top-0 last-of-type:bottom-0 last-of-type:left-0 -z-10 size-[min(50vmin,theme(maxWidth.md))] max-lg:hidden'>
+    <Icon
+      className='size-full text-muted'
+      {...data}
+    />
+    </div>
+  </ScrollAnimateTransform>)}
+  </div>
+}
 
 export default AboutViewValuesSection;
