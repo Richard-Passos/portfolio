6e249455691ee4be5c78821e58f778c6dd7f16d7
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-12 10:53:50 -0300

    CHORE add dynamic data

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,30 @@
+import { pagesApi } from '@/api';
+import { capitalize } from '@/utils';
 import { NotFoundView } from '@/views';
 
-const NotFoundPage = () => {
-  return <NotFoundView />;
+const NotFoundPage = async (props) => {
+  const { data } = await pagesApi.getOne('not-found');
+
+  return (
+    <NotFoundView
+      data={data}
+      {...props}
+    />
+  );
 };
 
-const metadata = {
-  title: 'Error',
+const generateMetadata = async () => {
+  const { metadata = {} } = (await pagesApi.getOne('not-found')).data || {};
+
+  return {
+    title: capitalize(metadata.title),
+    description: metadata.description,
+    openGraph: {
+      title: capitalize(metadata.title),
+      description: metadata.description,
+    },
+  };
 };
 
 export default NotFoundPage;
-export { metadata };
+export { generateMetadata };
