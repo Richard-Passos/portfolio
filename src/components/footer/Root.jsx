d5e46bb69133575cdbe20b25358f72bdb2c60b5f
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:15:31 -0300

    CHORE add pt-lg and lines to fill the space

--- src/components/footer/Root.jsx
+++ src/components/footer/Root.jsx
@@ -2,17 +2,22 @@ import { cn } from '@/utils';
 
 import Overlay from './Overlay';
 import Content from './content';
+import Lines from '../lines';
 
 const Footer = ({ className, ...props }) => {
   return (
     <footer
       className={cn(
-        'light relative flex w-full max-w-bounds flex-col items-center overflow-y-clip',
+        'relative flex w-full max-w-bounds flex-col items-center overflow-y-clip pt-lg',
         className,
       )}
       {...props}
     >
-      <Overlay theme='dark' />
+      <div className='dark absolute h-lg top-0 w-screen bg-main'>
+      <Lines className='z-0'/>
+      </div>
+
+      <Overlay theme='dark' className='top-lg' />
 
       <Content theme='light' />
     </footer>
