ef616618835fe7747167d1787c6ebd211452f52b
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-12 10:54:23 -0300

    CHORE export new constant

--- src/constants/pages/index.js
+++ src/constants/pages/index.js
@@ -2,6 +2,7 @@ import aboutPage from './about';
 import contactPage from './contact';
 import errorPage from './error';
 import homePage from './home';
+import notFoundPage from './notFound';
 import projectsPage from './projects';
 import workPage from './work';
 
@@ -10,9 +11,18 @@ const pages = [
   contactPage,
   errorPage,
   homePage,
+  notFoundPage,
   projectsPage,
   workPage,
 ];
 
 export default pages;
-export { aboutPage, contactPage, errorPage, homePage, projectsPage, workPage };
+export { 
+  aboutPage,
+   contactPage,
+   errorPage,
+   homePage,
+   notFoundPage,
+   projectsPage,
+   workPage 
+  };
