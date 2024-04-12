e493b3358f6d50fe6105572255e5bae9dfa8de44
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:08:05 -0300

    CHORE move NextPage to sections

---
+++ src/views/work/sections/NextPage.jsx
@@ -0,0 +1,15 @@
+import { NextPage, Section } from '@/components';
+import { cn } from '@/utils';
+
+const WorkViewNextPageSection = ({ className, data = {}, ...props }) => {
+  return (
+    <Section
+      className={cn('!min-h-0', className)}
+      {...props}
+    >
+      <NextPage data={data} />
+    </Section>
+  );
+};
+
+export default WorkViewNextPageSection;
