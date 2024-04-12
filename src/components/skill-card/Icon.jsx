f3bbc422f1a2609a938da2742c9ff5aec810f46f
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-11 22:19:56 -0300

    CHORE remove delay and text-content

--- src/components/skill-card/Icon.jsx
+++ src/components/skill-card/Icon.jsx
@@ -6,7 +6,7 @@ const SkillCardIcon = ({ className, children, ...props }) => {
   return (
     <div
       className={cn(
-        'absolute flex aspect-square h-2/3 items-center justify-center place-self-center rounded-md bg-muted/50 transition-[transform,opacity] delay-150 duration-300 ease-backOut group-data-open:-translate-y-2 group-data-open:opacity-0 group-data-open:delay-0 group-data-open:ease-in-out',
+        'absolute flex aspect-square h-2/3 items-center justify-center place-self-center rounded-md bg-muted/50 transition-[transform,opacity] delay-150 duration-300 group-data-open:-translate-y-2 group-data-open:opacity-0 group-data-open:delay-0 group-data-open:ease-in-out',
         className,
       )}
       {...props}
--- src/components/skill-card/WaterMark.jsx
+++ src/components/skill-card/WaterMark.jsx
@@ -6,7 +6,7 @@ const SkillCardWaterMark = ({ className, ...props }) => {
   return (
     <Slot
       className={cn(
-        'absolute right-0 top-0 -z-10 size-2/3 -translate-y-[--translate] translate-x-[--translate] opacity-[--opacity] transition-[transform,opacity] duration-300 ease-backOut [--opacity:0] [--translate:50%] group-data-open:[--opacity:.025] group-data-open:[--translate:25%] dark:group-data-open:[--opacity:.01]',
+        'absolute right-0 top-0 -z-10 size-2/3 -translate-y-[--translate] translate-x-[--translate] opacity-[--opacity] transition-[transform,opacity] duration-300 [--opacity:0] [--translate:50%] group-data-open:[--opacity:.025] group-data-open:[--translate:25%] dark:group-data-open:[--opacity:.01]',
         className,
       )}
       {...props}
