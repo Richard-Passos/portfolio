ba0ec9a734c814415024d894827129690ef6fc36
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-12 10:58:25 -0300

    CHORE get dynamic data

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,13 +1,30 @@
+import { pagesApi } from '@/api';
+import { capitalize } from '@/utils';
 import { ProjectsView } from '@/views';
 
-const ProjectsPage = () => {
-  return <ProjectsView />;
+const ProjectsPage = async (props) => {
+  const { data } = await pagesApi.getOne('projects');
+
+  return (
+    <ProjectsView
+      data={data}
+      {...props}
+    />
+  );
 };
 
-const metadata = {
-  title: 'Projects',
-  description: 'Richard passos projects.',
+const generateMetadata = async () => {
+  const { metadata = {} } = (await pagesApi.getOne('projects')).data || {};
+
+  return {
+    title: capitalize(metadata.title),
+    description: metadata.description,
+    openGraph: {
+      title: capitalize(metadata.title),
+      description: metadata.description,
+    },
+  };
 };
 
 export default ProjectsPage;
-export { metadata };
+export { generateMetadata };
