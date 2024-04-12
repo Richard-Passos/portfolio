91beb3192f6f876161168a8d33533bebc0037ac6
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-12 10:54:51 -0300

    FEAT create NotFoundPage data

---
+++ src/constants/pages/notFound.js
@@ -0,0 +1,29 @@
+const notFoundPage = {
+  slug: 'not-found',
+  sections: [
+    {
+      slug: 'hero',
+      theme: 'dark',
+      data: {
+        title: '404',
+        subtitle: 'Page not found!',
+        description: "Ops... looks like you're lost. Please go back to Home page.",
+        actions: [
+          {
+            type: '',
+            data: {
+              href: '/',
+              label: 'Back Home',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  metadata: {
+    title: 'Error',
+    description: 'Not found page on Richatd Passos portfolio.'
+  }
+}
+
+export default notFoundPage
