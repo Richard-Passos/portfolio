f454dd16a2c8eb787917a803e6ba0faffcb31d1e
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-14 00:55:06 -0300

    CHORE add footer data

--- src/constants/globals.js
+++ src/constants/globals.js
@@ -1,209 +1,263 @@
 const globals = [
-  {},
   {
     slug: 'header',
-    navItems: [
-      {
-        type: '',
-        data: {
-          href: '/',
-          label: 'Home',
+    data: {
+      navItems: [
+        {
+          type: '',
+          data: {
+            href: '/',
+            label: 'Home',
+          }
+        },
+        {
+          type: '',
+          data: {
+            href: '/work',
+            label: 'Work',
+          }
+        },
+        {
+          type: '',
+          data: {
+            href: '/about',
+            label: 'About',
+          }
+        },
+        {
+          type: '',
+          data: {
+            href: '/contact',
+            label: 'Contact',
+          }
+        },
+      ],
+    }
+  },
+  {
+    slug: 'footer',
+    theme: 'light',
+    data: {
+      cta: {
+        title: `Let's work \ntogether`,
+        subtitle: 'Is your great ideas ready to fly?',
+        action: {
+          type: '',
+          icon: {
+            src: '/images/arrow-up-right.svg'
+          },
+          data: {
+            href: '/contact',
+            label: 'Go to contact page',
+            variants: {
+              size: 'lg'
+            }
+          }
         }
       },
-      {
-        type: '',
-        data: {
-          href: '/work',
-          label: 'Work',
-        }
-      },
-      {
-        type: '',
-        data: {
-          href: '/about',
-          label: 'About',
-        }
-      },
-      {
-        type: '',
-        data: {
-          href: '/contact',
-          label: 'Contact',
-        }
-      },
-    ],
+      personal: {
+        smalls: [
+          {
+            href: 'https://github.com/Richard-Passos',
+            label: 'Made with ❤️ by <a>Richard</a>'
+          },
+          {
+            href: 'https://www.linkedin.com/in/richardp-dev/',
+            label: '© 2024 <a>Richard Passos</a>'
+          }
+        ],
+        action: {
+          type: '',
+          icon: {
+            src: '/images/arrow-up.svg'
+          },
+          animation: 'slideUp',
+          data: {
+            label: 'Back top',
+            variants: {
+              color: 'main', 
+              size: 'sm'
+            }
+          }
+        },
+        socialsTitle: 'Socials'
+      }
+    }
   },
   {
     slug: 'personal-info',
-    name: {
-      first: 'Richard',
-      last: 'Passos',
-    },
-    email: 'hi.richardp@gmail.com',
-    job: 'Full stack developer',
-    location: {
-      country: 'Brazil',
-      state: 'RS',
-      gmt: 'GMT-3',
-    },
-    logo: {
-      src: '/images/logo.svg',
-      alt: 'Richard Passos logo',
-      width: 220,
-      height: 110,
-    },
-    socials: [
-      {
-        icon: { src: '/images/github.svg' },
-        label: 'Github',
-        href: 'https://github.com/Richard-Passos',
+    data: {
+      name: {
+        first: 'Richard',
+        last: 'Passos',
       },
-      {
-        icon: { src: '/images/instagram.svg' },
-        label: 'Instagram',
-        href: 'https://www.instagram.com/richardp.dev',
+      description: 'An great full stack developer to fit your needs.',
+      email: 'hi.richardp@gmail.com',
+      job: 'Full stack developer',
+      location: {
+        country: 'Brazil',
+        state: 'RS',
+        gmt: 'GMT-3',
       },
-      {
-        icon: { src: '/images/linkedin.svg' },
-        label: 'Linkedin',
-        href: 'https://www.linkedin.com/in/richardp-dev',
+      logo: {
+        src: '/images/logo.svg',
+        alt: 'Richard Passos logo',
+        width: 220,
+        height: 110,
       },
-    ],
-    buyCoffee: {
-      label: 'Buy me a coffee',
-      href: 'https://www.buymeacoffee.com/richardp.dev',
-    },
-    availability: 'Available to work',
-    services: [
-      {
-        title: 'Front end',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio corporis sequi, dolores nostrum, praesentium omnis commodi sint enim ducimus molestiae necessitatibus consequatur et fugiat nobis magnam asperiores culpa harum?',
-      },
-      {
-        title: 'Back end',
-        description:
-          'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem culpa dolor vel rerum! Quaerat delectus earum tempore accusamus itaque mollitia asperiores voluptate ea. Labore, officiis quae ad dolorum ea temporibus.',
-      },
-      {
-        title: 'Full stack',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad delectus dolore, veniam accusamus beatae nulla sequi numquam alias rerum vel, distinctio vero dolor necessitatibus commodi cum debitis porro, maxime ratione.',
-      },
-    ],
-    stats: [
-      {
-        title: '10ᐩ',
-        description: `Projects \ncompleted`,
-      },
-      {
-        title: '1ᐩ',
-        description: `Years of \nexperience`,
-      },
-      {
-        title: '100%',
-        description: `Great \nprojects`,
-      },
-      {
-        title: '50ᐩ',
-        description: `Clebers \ncompleted`,
-      },
-    ],
-    values: {
-      work: [
+      socials: [
         {
-          icon: {
-            src: '/images/shield.svg',
-            alt: 'Integrity icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Integrity',
-          description:
-            'I remain true to my values, striving to act with honesty and transparency. This allows me to build solid and genuine relationships.',
+          icon: { src: '/images/github.svg' },
+          label: 'Github',
+          href: 'https://github.com/Richard-Passos',
         },
         {
-          icon: {
-            src: '/images/globe.svg',
-            alt: 'Curiosity icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Curiosity',
-          description:
-            'My innate curiosity drives me to explore new ideas and discover new paths. Every challenge is an opportunity for learning and personal growth.',
+          icon: { src: '/images/instagram.svg' },
+          label: 'Instagram',
+          href: 'https://www.instagram.com/richardp.dev',
         },
         {
-          icon: {
-            src: '/images/puzzle.svg',
-            alt: 'Respect icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Respect',
-          description:
-            'I deeply value the people around me and their unique stories. I am always open to listening and learning from others, recognizing the importance of each voice and experience.',
-        },
-        {
-          icon: {
-            src: '/images/smile.svg',
-            alt: 'Gratitude icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Gratitude',
-          description:
-            'I cultivate a mindset of gratitude, recognizing and appreciating the little things in life. Gratitude keeps me focused and inspires me to live with purpose and fulfillment.',
+          icon: { src: '/images/linkedin.svg' },
+          label: 'Linkedin',
+          href: 'https://www.linkedin.com/in/richardp-dev',
         },
       ],
-      personal: [
+      buyCoffee: {
+        label: 'Buy me a coffee',
+        href: 'https://www.buymeacoffee.com/richardp.dev',
+      },
+      availability: 'Available to work',
+      services: [
         {
-          icon: {
-            src: '/images/shield.svg',
-            alt: 'Integrity icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Integrity',
+          title: 'Front end',
           description:
-            'I remain true to my values, striving to act with honesty and transparency. This allows me to build solid and genuine relationships.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non optio corporis sequi, dolores nostrum, praesentium omnis commodi sint enim ducimus molestiae necessitatibus consequatur et fugiat nobis magnam asperiores culpa harum?',
         },
         {
-          icon: {
-            src: '/images/globe.svg',
-            alt: 'Curiosity icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Curiosity',
+          title: 'Back end',
           description:
-            'My innate curiosity drives me to explore new ideas and discover new paths. Every challenge is an opportunity for learning and personal growth.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem culpa dolor vel rerum! Quaerat delectus earum tempore accusamus itaque mollitia asperiores voluptate ea. Labore, officiis quae ad dolorum ea temporibus.',
         },
         {
-          icon: {
-            src: '/images/puzzle.svg',
-            alt: 'Respect icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Respect',
+          title: 'Full stack',
           description:
-            'I deeply value the people around me and their unique stories. I am always open to listening and learning from others, recognizing the importance of each voice and experience.',
-        },
-        {
-          icon: {
-            src: '/images/smile.svg',
-            alt: 'Gratitude icon',
-            width: 150,
-            height: 150,
-          },
-          title: 'Gratitude',
-          description:
-            'I cultivate a mindset of gratitude, recognizing and appreciating the little things in life. Gratitude keeps me focused and inspires me to live with purpose and fulfillment.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad delectus dolore, veniam accusamus beatae nulla sequi numquam alias rerum vel, distinctio vero dolor necessitatibus commodi cum debitis porro, maxime ratione.',
         },
       ],
-    },
-  },
+      stats: [
+        {
+          title: '10ᐩ',
+          description: `Projects \ncompleted`,
+        },
+        {
+          title: '1ᐩ',
+          description: `Years of \nexperience`,
+        },
+        {
+          title: '100%',
+          description: `Great \nprojects`,
+        },
+        {
+          title: '50ᐩ',
+          description: `Clebers \ncompleted`,
+        },
+      ],
+      values: {
+        work: [
+          {
+            icon: {
+              src: '/images/shield.svg',
+              alt: 'Integrity icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Integrity',
+            description:
+              'I remain true to my values, striving to act with honesty and transparency. This allows me to build solid and genuine relationships.',
+          },
+          {
+            icon: {
+              src: '/images/globe.svg',
+              alt: 'Curiosity icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Curiosity',
+            description:
+              'My innate curiosity drives me to explore new ideas and discover new paths. Every challenge is an opportunity for learning and personal growth.',
+          },
+          {
+            icon: {
+              src: '/images/puzzle.svg',
+              alt: 'Respect icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Respect',
+            description:
+              'I deeply value the people around me and their unique stories. I am always open to listening and learning from others, recognizing the importance of each voice and experience.',
+          },
+          {
+            icon: {
+              src: '/images/smile.svg',
+              alt: 'Gratitude icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Gratitude',
+            description:
+              'I cultivate a mindset of gratitude, recognizing and appreciating the little things in life. Gratitude keeps me focused and inspires me to live with purpose and fulfillment.',
+          },
+        ],
+        personal: [
+          {
+            icon: {
+              src: '/images/shield.svg',
+              alt: 'Integrity icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Integrity',
+            description:
+              'I remain true to my values, striving to act with honesty and transparency. This allows me to build solid and genuine relationships.',
+          },
+          {
+            icon: {
+              src: '/images/globe.svg',
+              alt: 'Curiosity icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Curiosity',
+            description:
+              'My innate curiosity drives me to explore new ideas and discover new paths. Every challenge is an opportunity for learning and personal growth.',
+          },
+          {
+            icon: {
+              src: '/images/puzzle.svg',
+              alt: 'Respect icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Respect',
+            description:
+              'I deeply value the people around me and their unique stories. I am always open to listening and learning from others, recognizing the importance of each voice and experience.',
+          },
+          {
+            icon: {
+              src: '/images/smile.svg',
+              alt: 'Gratitude icon',
+              width: 150,
+              height: 150,
+            },
+            title: 'Gratitude',
+            description:
+              'I cultivate a mindset of gratitude, recognizing and appreciating the little things in life. Gratitude keeps me focused and inspires me to live with purpose and fulfillment.',
+          },
+        ],
+      },
+    }
+  }
 ];
 
 export default globals;
