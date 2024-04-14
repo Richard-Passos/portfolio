6a544df3e7b805de3e7c634ab30b31ee9925cd81
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-14 20:58:18 -0300

    CHORE move

---
+++ src/views/project/sections/NextPage.jsx
@@ -0,0 +1,26 @@
+import { projectsApi } from '@/api';
+import { NextPage, Section } from '@/components';
+import { cn } from '@/utils';
+
+const ProjectViewNextPageSection = async ({ project = {}, className, data = {}, ...props }) => {
+  const { adjacentIds = {} } = project;
+
+  const nextProject = (await projectsApi.getOne(adjacentIds.next)).data || {}
+
+  data = {
+    ...data, 
+    description: `${nextProject.title} ·`,
+    image: nextProject.thumbnail
+  }
+
+  return (
+    <Section
+      className={cn('!min-h-0', className)}
+      {...props}
+    >
+      <NextPage data={data} />
+    </Section>
+  );
+};
+
+export default ProjectViewNextPageSection;
