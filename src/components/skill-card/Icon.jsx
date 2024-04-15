0d7f9daa78352fda81b28fd02624789853d5d480
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-15 20:24:37 -0300

    CHORE from h to size

--- src/components/skill-card/Icon.jsx
+++ src/components/skill-card/Icon.jsx
@@ -4,7 +4,7 @@ const SkillCardIcon = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        'absolute flex aspect-square h-2/3 items-center justify-center place-self-center rounded-sm bg-muted/40 transition-[transform,opacity] delay-150 duration-300 group-data-open:-translate-y-2 group-data-open:opacity-0 group-data-open:delay-0 dark:bg-muted/10 [&>svg]:h-2/3',
+        'absolute flex aspect-square h-2/3 items-center justify-center place-self-center rounded-sm bg-muted/40 transition-[transform,opacity] delay-150 duration-300 group-data-open:-translate-y-2 group-data-open:opacity-0 group-data-open:delay-0 dark:bg-muted/10 [&>svg]:size-2/3',
         className,
       )}
       {...props}
