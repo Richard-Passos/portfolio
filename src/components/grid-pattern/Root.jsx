2d17bd8b1b885583a2510fc197a2d77c19a084c4
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-22 10:26:04 -0300

    FIX opacity var

--- src/components/grid-pattern/Root.jsx
+++ src/components/grid-pattern/Root.jsx
@@ -4,7 +4,7 @@ const GridPattern = ({ className, ...props }) => {
   return (
     <span
       className={cn(
-        'absolute inset-0 -z-40 overflow-hidden border bg-main bg-center [--c:hsl(var(--color)/var(--opacity))] [--color:--border] [--opacity:theme(opacity.60)] [background-image:linear-gradient(var(--c)_.8px,transparent_.8px),linear-gradient(to_right,var(--c)_.8px,transparent_.8px)] [background-size:60px_60px] [border-color:--c] dark:[---opacity:theme(opacity.20)]',
+        'absolute inset-0 -z-40 overflow-hidden border bg-main bg-center [--c:hsl(var(--color)/var(--opacity))] [--color:--border] [--opacity:theme(opacity.60)] [background-image:linear-gradient(var(--c)_.8px,transparent_.8px),linear-gradient(to_right,var(--c)_.8px,transparent_.8px)] [background-size:60px_60px] [border-color:--c] dark:[--opacity:theme(opacity.20)]',
         className,
       )}
       {...props}
