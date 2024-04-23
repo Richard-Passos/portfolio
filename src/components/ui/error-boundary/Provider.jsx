a516325a7f853108052c1d815d24e7143f8bfdcd
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-23 10:57:51 -0300

    CHORE add lang

--- src/components/ui/error-boundary/Provider.jsx
+++ src/components/ui/error-boundary/Provider.jsx
@@ -1,8 +1,8 @@
 import { pagesApi } from '@/api';
 import { ErrorProvider } from '@/contexts';
 
-const ErrorBoundaryProvider = async ({ value, ...props }) => {
-  const { data } = await pagesApi.getOne('error');
+const ErrorBoundaryProvider = async ({ lang, value, ...props }) => {
+  const { data } = await pagesApi.getOne('error', `?lang=${lang}`);
 
   return (
     <ErrorProvider
