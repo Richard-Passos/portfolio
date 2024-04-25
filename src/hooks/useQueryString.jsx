01d04d4ee79c1e023f23531a54fec3314e4b5ae4
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 20:05:57 -0300

    CHORE from Â[] to {}

--- src/hooks/useQueryString.jsx
+++ src/hooks/useQueryString.jsx
@@ -3,12 +3,12 @@
 import { useSearchParams } from 'next/navigation';
 import { useCallback } from 'react';
 
-const useQueryString = (querys = []) => {
+const useQueryString = (querys = {}) => {
   const searchParams = useSearchParams();
 
   const createQueryString = useCallback(() => {
     const params = new URLSearchParams(searchParams.toString());
-    querys.forEach(([name, value]) => params.set(name, value));
+    Object.entries(querys).forEach(([key, value]) => params.set(key, value))
 
     return params.toString();
   }, [querys, searchParams]);
