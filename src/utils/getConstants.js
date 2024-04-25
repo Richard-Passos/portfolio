50afbcd21345dc3483707324599b844367afb47c
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 20:06:38 -0300

    CHORE get locales from @/navigation

--- src/utils/getConstants.js
+++ src/utils/getConstants.js
@@ -1,7 +1,8 @@
 import * as constants from '@/constants'
 import normId from './normId'
+import { locales } from '@/navigation'
 
-const getConstants = (locale) => constants[normId(locale)] || constants[normId(constants.locales?.[0])] || constants
+const getConstants = (locale) => constants[normId(locale)] || constants[normId(locales[0])] || constants
 
 
 
