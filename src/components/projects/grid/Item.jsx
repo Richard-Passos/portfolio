27da67aad4196d78902146c4a454610aea859836
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 20:01:40 -0300

    CHORE remove div and move className to parent

--- src/components/projects/grid/Item.jsx
+++ src/components/projects/grid/Item.jsx
@@ -6,7 +6,7 @@ const ProjectsGridItem = ({ className, content, children, ...props }) => {
   return (
     <CursorLink
       className={cn(
-        'group aspect-square w-full rounded-3xl no-underline md:even:mt-xl md:[&:not(:last-child)]:even:-mb-xl',
+        'group aspect-square w-full relative grid grid-cols-4 items-end overflow-hidden p-sm transition-transform duration-300 group-hover:scale-95 rounded-3xl no-underline md:even:mt-xl md:[&:not(:last-child)]:even:-mb-xl',
         className,
       )}
       content={{
@@ -16,11 +16,9 @@ const ProjectsGridItem = ({ className, content, children, ...props }) => {
       }}
       {...props}
     >
-      <div className='relative grid size-full grid-cols-4 items-end overflow-hidden rounded-inherit p-sm transition-transform duration-300 group-hover:scale-95'>
         {children}
 
         <span className='absolute inset-0 rounded-inherit bg-main/60 opacity-0 transition-[transform,opacity] duration-300 group-hover:opacity-100' />
-      </div>
     </CursorLink>
   );
 };
