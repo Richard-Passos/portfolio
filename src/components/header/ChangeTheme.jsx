92483ad2e605c7d8e8debc5ba899ab84454429e5
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 19:54:08 -0300

    FIX with useUpdateMotionValueEvent

--- src/components/header/ChangeTheme.jsx
+++ src/components/header/ChangeTheme.jsx
@@ -1,47 +1,44 @@
 'use client';
 
 import { Slot } from '@radix-ui/react-slot';
-import { useMotionValueEvent, useScroll } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { useScroll } from 'framer-motion';
+import { useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { useWindowSize } from '@/hooks';
+import { useUpdateMotionValueEvent, useWindowSize } from '@/hooks';
 import { setHeaderTheme } from '@/redux';
 
 const HeaderChangeTheme = ({ theme, force, ...props }) => {
-  const header = useSelector((data) => data.header),
+  const {height, theme: activeTheme} = useSelector((data) => data.header),
     windowSize = useWindowSize(),
     dispatch = useDispatch(),
-    ref = useRef(null),
-    hasRendered = useRef(false);
+    ref = useRef(null);
 
-  const handleSetTheme = (y) => {
-    if (hasRendered.current) {
-      if (y === 1 && header.theme !== theme) dispatch(setHeaderTheme(theme));
-    } else {
-      hasRendered.current = true;
-    }
-  };
-
-  const limitY = windowSize.height / (header.height * 100);
+  const limitY = windowSize.height / (height * 100);
 
   const { scrollYProgress: y1 } = useScroll({
     target: ref,
     offset: [`0 ${limitY + 0.001}`, `0 ${limitY}`],
-  });
-
-  useMotionValueEvent(y1, 'change', handleSetTheme);
-
-  const { scrollYProgress: y2 } = useScroll({
+  }),
+  { scrollYProgress: y2 } = useScroll({
     target: ref,
     offset: [`1 ${limitY - 0.001}`, `1 ${limitY}`],
   });
 
-  useMotionValueEvent(y2, 'change', handleSetTheme);
+  const handleSetTheme = useCallback(() => {
+    if (activeTheme !== theme) dispatch(setHeaderTheme(theme))
+  }, [activeTheme, theme, dispatch, setHeaderTheme])
 
-  useEffect(() => {
-    if (force) dispatch(setHeaderTheme(theme));
-  }, [force, dispatch, theme]);
+  const onChange = (y) => {
+    if(y === 1) handleSetTheme()
+  }; 
+
+  useUpdateMotionValueEvent(y1, 'change', onChange)
+  useUpdateMotionValueEvent(y2, 'change', onChange)
+
+    useEffect(() => {
+      if (force) handleSetTheme()
+    }, [force]);
 
   return (
     <Slot
