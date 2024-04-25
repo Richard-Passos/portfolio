17b03a8eda00ffb9e7b6aaf813fcbefe4e75868d
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 20:08:22 -0300

    CHORE rename forceHeaderTheme to forceTheme

--- src/views/not-found/Root.jsx
+++ src/views/not-found/Root.jsx
@@ -12,7 +12,7 @@ const NotFoundView = ({ data = {} }) => {
 
     Section = Section && (
       <Section
-        id={slug}
+      key={slug}
         hasTransition={
           slug.toLowerCase() !== 'hero' && lastTheme !== data.theme
         }
