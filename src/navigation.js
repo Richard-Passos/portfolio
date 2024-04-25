236c7f161d0e7fbe049cd50b8c76fc5075fea34c
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-25 19:47:40 -0300

    FEAT add navigation file from next-intrl

---
+++ src/navigation.js
@@ -0,0 +1,9 @@
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+
+const locales = ['en', 'pt'];
+
+const sharedPathnamesNavigation =
+createSharedPathnamesNavigation({ locales });
+  
+export { locales }
+export const { Link, redirect, usePathname, useRouter } = sharedPathnamesNavigation
