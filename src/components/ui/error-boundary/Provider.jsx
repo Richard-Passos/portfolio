f8b849b290c5a955f0dd39bc53bcd066eeaa1ce5
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-29 15:22:52 -0300

    CHORE remove unnecessary locale

--- src/components/ui/error-boundary/Provider.jsx
+++ src/components/ui/error-boundary/Provider.jsx
@@ -1,11 +1,8 @@
 import { pagesApi } from '@/api';
 import { ErrorProvider } from '@/contexts';
-import { getLocale } from 'next-intl/server';
 
 const ErrorBoundaryProvider = async ({ value, ...props }) => {
-  const locale = await getLocale()
-
-  const { data } = await pagesApi.getOne('error', `?locale=${locale}`);
+  const { data } = await pagesApi.getOne('error');
 
   return (
     <ErrorProvider
