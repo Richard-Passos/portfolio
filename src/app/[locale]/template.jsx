b52ecbcc3995e906d4cf244043189196722b71f8
Richard Passos <richard.ac.passos12@gmail.com>
2024-04-29 15:19:36 -0300

    CHORE add headerGetState to fix dark mode

--- src/app/[locale]/template.jsx
+++ src/app/[locale]/template.jsx
@@ -1,4 +1,5 @@
 import { Animate } from '@/components';
+import { HeaderGetState } from '@/components/header';
 
 const ANIMATION_VARIANTS = [
   { y: '7.5rem', opacity: 0 },
@@ -13,9 +14,11 @@ const Template = ({ children, ...props }) => {
       transition={{ duration: 0.5 }}
       {...props}
     >
+      <HeaderGetState>
       <main className='relative flex w-full max-w-bounds flex-col items-center'>
         {children}
       </main>
+      </HeaderGetState>
     </Animate>
   );
 };
