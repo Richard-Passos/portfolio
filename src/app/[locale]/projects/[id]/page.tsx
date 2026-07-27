ee8688afc3d024374afaaf760458223e8023dd61
Richard Passos <richard.ac.passos12@gmail.com>
2026-07-27 20:00:18 -0300

    FIX project page by adding header, footer and i18n provider

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -2,13 +2,12 @@ import { PROJECTS } from '@/common/PROJECTS';
 import { notFound } from 'next/navigation';
 import { AdjacentProjects } from '@/components/layout/Blocks/AdjacentProjects';
 import { ProjectHero } from '@/components/layout/Heros/Project';
+import { NextPageIntlayer } from 'next-intlayer';
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
+import { IntlayerServerProvider } from 'next-intlayer/server';
 
-export type ProjectPageProps = {
-  params: Promise<{ id: string }>;
-};
-
-const ProjectPage = async ({ params }: ProjectPageProps) => {
-  const { id } = await params;
+const ProjectContent = ({ id }: { id: string }) => {
   const project = PROJECTS.find((p) => p.id == id);
 
   if (!project) notFound();
@@ -27,6 +26,24 @@ const ProjectPage = async ({ params }: ProjectPageProps) => {
         theme='dark'
         data={{ prev, next }}
       />
+    </>
+  );
+};
+
+const ProjectPage: NextPageIntlayer<{ id: string }> = async ({ params }) => {
+  const { locale, id } = await params;
+
+  return (
+    <>
+      <IntlayerServerProvider locale={locale}>
+        <Header />
+
+        <main className='z-10 flex w-full max-w-bounds grow flex-col items-center justify-center'>
+          <ProjectContent id={id} />
+        </main>
+
+        <Footer />
+      </IntlayerServerProvider>
     </>
   );
 };
